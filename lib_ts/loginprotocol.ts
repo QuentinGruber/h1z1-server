c28c6d3055495a804198458e75ba65a7a620e0d2
Quentin Gruber <quentingruber@gmail.com>
2020-08-20 15:21:40 +0200

    add spamglitch as an option for loginprotocol & rewrite it in ts

---
+++ lib_ts/loginprotocol.ts
@@ -0,0 +1,74 @@
+var fs = require("fs"),
+  debug = require("debug")("LoginProtocol"),
+  DataSchema = require("h1z1-dataschema"),
+  LoginPackets = require("./loginpackets");
+
+export class LoginProtocol {
+  _SpamGlitch: boolean;
+  constructor(SpamGlitch: boolean) {
+    var n = 0;
+    this._SpamGlitch = SpamGlitch;
+  }
+  parse(data: any) {
+    var packetType = data[0],
+      result,
+      schema,
+      name,
+      packet = LoginPackets.Packets[packetType];
+    console.log("deqsfezsf spam glitch = ");
+    console.log(this);
+    if (packet == undefined && this._SpamGlitch)
+      packet = LoginPackets.Packets[1]; // HACK
+    if (packet) {
+      if (packet.schema) {
+        debug(packet.name);
+        result = DataSchema.parse(packet.schema, data, 1).result;
+        debug("[DEBUG] LoginRequestPacket :");
+        debug(result);
+
+        return {
+          type: packet.type,
+          name: packet.name,
+          result: result,
+        };
+      } else {
+        debug("parse()", "No schema for packet ", packet.name);
+        return false;
+      }
+    } else {
+      //fs.writeFileSync("debug/loginbadpacket.dat", data); Diseable that
+      debug(
+        "parse() " + "Unknown or unhandled login packet type: " + packetType
+      );
+      return false;
+    }
+  }
+
+  pack(packetName: string, object: any) {
+    var packetType = LoginPackets.PacketTypes[packetName],
+      packet = LoginPackets.Packets[packetType],
+      payload,
+      data;
+    if (packet) {
+      if (packet.schema) {
+        debug("Packing data for " + packet.name);
+        payload = DataSchema.pack(packet.schema, object);
+        if (payload) {
+          data = Buffer.alloc(1 + payload.length); // remove "new" maybe creating issues
+          data.writeUInt8(packetType, 0);
+          payload.data.copy(data, 1);
+        } else {
+          debug("Could not pack data schema for " + packet.name);
+        }
+      } else {
+        debug("pack()", "No schema for packet " + packet.name);
+      }
+    } else {
+      debug("pack()", "Unknown or unhandled login packet type: " + packetType);
+    }
+    return data;
+  }
+}
+
+exports.LoginProtocol = LoginProtocol;
+exports.LoginPackets = LoginPackets;
