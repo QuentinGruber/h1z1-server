c8ef29e95838560dcee6cbd218551c8a5bdd30fa
Quentin Gruber <quentingruber@gmail.com>
2020-10-07 18:17:48 +0200

    clean stuff before merge

--- lib_ts/zoneserver.js
+++ lib_ts/zoneserver.js
@@ -109,7 +109,7 @@ function ZoneServer(serverPort, gatewayKey, UsingMongo) {
           unknownDword13: 0,
           unknownDword14: 0,
           unknownDword15: 0,
-          unknownDword16: 50,// sun orientation on x axis ???
+          unknownDword16: 0,// sun orientation on x axis ???
           unknownDword17: 0, // night when 100
           unknownDword18: 0, 
           unknownDword19: 0,
