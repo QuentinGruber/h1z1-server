0ded914d8c80d2a8e71982f65300c2c7b49bec6b
Quentin Gruber <quentingruber@gmail.com>
2020-11-03 13:11:00 +0100

    delete /lib

--- lib/loginprotocol.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class LoginProtocol {
-    parse(data: any): false | {
-        type: any;
-        name: any;
-        result: {};
-    };
-    pack(packetName: string, object: any): any;
-}
--- lib/loginproxy.d.ts
+++
@@ -1,9 +0,0 @@
-export var LoginProxy: typeof LoginProxy;
-declare function LoginProxy(remoteAddress: any, remotePort: any, cryptoKey: any, localPort: any, localClientPort: any): void;
-declare class LoginProxy {
-    constructor(remoteAddress: any, remotePort: any, cryptoKey: any, localPort: any, localClientPort: any);
-    _soeProxy: any;
-    start(): void;
-    stop(): void;
-}
-export {};
