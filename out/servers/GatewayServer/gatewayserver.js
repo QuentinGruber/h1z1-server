46dd47ac5b3764800d883f097a817a0091189b6e
Quentin Gruber <quentingruber@gmail.com>
2020-12-06 18:17:40 +0100

    change crc seed for gateway + add know fields to sendself

--- out/servers/GatewayServer/gatewayserver.js
+++ out/servers/GatewayServer/gatewayserver.js
@@ -34,7 +34,7 @@ var GatewayServer = /** @class */ (function (_super) {
     function GatewayServer(protocolName, serverPort, gatewayKey) {
         var _this = _super.call(this) || this;
         _this._compression = 0x0000;
-        _this._crcSeed = (13049251224421725568).toString(16);
+        _this._crcSeed = 0;
         _this._crcLength = 2;
         _this._udpLength = 512;
         _this._soeServer = new soeserver_1.SOEServer(protocolName, serverPort, gatewayKey, _this._compression, true, false // use crc64
