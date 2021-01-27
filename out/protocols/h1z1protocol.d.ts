d50911517247d728305a51eab2ac7e3e577e2ee0
Quentin Gruber <quentingruber@gmail.com>
2021-01-27 15:17:46 +0100

    cleaning /out

--- out/protocols/h1z1protocol.d.ts
+++ out/protocols/h1z1protocol.d.ts
@@ -1,1 +1,51 @@
+/// <reference types="node" />
+interface UpdatePositionObject {
+    flags: any;
+    unknown2_int32: any;
+    unknown3_int8: any;
+    unknown4: any;
+    position: any;
+    unknown6_int32: any;
+    unknown7_float: any;
+    unknown8_float: any;
+    unknown9_float: any;
+    unknown10_float: any;
+    unknown11_float: any;
+    unknown12_float: any;
+    unknown13_float: any;
+    unknown14_float: any;
+    unknown15_float: any;
+}
+interface PositionZoneToClient {
+    unknown1_uint: number;
+    positionData: UpdatePositionObject;
+}
+export declare class H1Z1Protocol {
+    H1Z1Packets: any;
+    protocolName: String;
+    constructor(protocolName?: String);
+    parseFacilityReferenceData(data: Buffer): {};
+    parseWeaponDefinitionReferenceData(data: Buffer): {} | undefined;
+    readUnsignedIntWith2bitLengthValue(data: Buffer, offset: number): {
+        value: number;
+        length: number;
+    };
+    readSignedIntWith2bitLengthValue(data: Buffer, offset: number): {
+        value: number;
+        length: number;
+    };
+    parseUpdatePositionData(data: Buffer, offset: number): UpdatePositionObject;
+    parseUpdatePositionClientToZone(data: Buffer, offset: number): {
+        result: UpdatePositionObject;
+    };
+    parseUpdatePositionZoneToClient(data: Buffer, offset: number): {
+        result: PositionZoneToClient;
+    };
+    pack(packetName: string, object: Buffer, referenceData: any): any;
+    parse(data: Buffer, flags: any, fromClient: boolean, referenceData: any): {
+        name: any;
+        data: any;
+    } | undefined;
+    reloadPacketDefinitions(): void;
+}
 export {};
