8d00320b2acc716046a7776244a86dd9b5db99e5
Quentin Gruber <quentingruber@gmail.com>
2021-01-27 14:34:17 +0100

    move lz4_decompress to utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -14,3 +14,60 @@ export const generateGuid = function (guidList: Array<string> = []) {
   } while (!_.indexOf(guidList, guid));
   return guid;
 };
+
+export const lz4_decompress = function (data: any, inSize: number, outSize: number) {
+  var outdata = new (Buffer as any).alloc(outSize),
+    token,
+    literalLength,
+    matchLength,
+    matchOffset,
+    matchStart,
+    matchEnd,
+    offsetIn = 0,
+    offsetOut = 0;
+
+  while (1) {
+    var token: any = data[offsetIn];
+    var literalLength: any = token >> 4;
+    var matchLength: any = token & 0xf;
+    offsetIn++;
+    if (literalLength) {
+      if (literalLength == 0xf) {
+        while (data[offsetIn] == 0xff) {
+          literalLength += 0xff;
+          offsetIn++;
+        }
+        literalLength += data[offsetIn];
+        offsetIn++;
+      }
+      data.copy(outdata, offsetOut, offsetIn, offsetIn + literalLength);
+
+      offsetIn += literalLength;
+      offsetOut += literalLength;
+    }
+
+    if (offsetIn < data.length - 2) {
+      var matchOffset = data.readUInt16LE(offsetIn);
+      offsetIn += 2;
+
+      if (matchLength == 0xf) {
+        while (data[offsetIn] == 0xff) {
+          matchLength += 0xff;
+          offsetIn++;
+        }
+        matchLength += data[offsetIn];
+        offsetIn++;
+      }
+      matchLength += 4;
+      var matchStart: any = offsetOut - matchOffset;
+      var matchEnd: any = offsetOut - matchOffset + matchLength;
+      for (var i = matchStart; i < matchEnd; i++) {
+        outdata[offsetOut] = outdata[i];
+        offsetOut++;
+      }
+    } else {
+      break;
+    }
+  }
+  return outdata;
+}
