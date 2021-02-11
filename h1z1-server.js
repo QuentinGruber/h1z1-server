9922d67be38a525f82439e918070cce7ce2e4146
Quentin Gruber <quentingruber@gmail.com>
2021-02-11 02:52:14 +0100

    Revert "Update h1z1-server.js"
    This reverts commit e0dac6b77ac765d94eca24f5eedcbd862b5f2151.

--- h1z1-server.js
+++ h1z1-server.js
@@ -14,27 +14,27 @@ const PackageSetting = require("./package.json");
 
 console.log(PackageSetting.name + " V" + PackageSetting.version);
 
-exports.SOEInputStream = require("./out/src/servers/SoeServer/soeinputstream.js").SOEInputStream;
-exports.SOEOutputStream = require("./out/src/servers/SoeServer/soeoutputstream.js").SOEOutputStream;
+exports.SOEInputStream = require("./out/servers/SoeServer/soeinputstream.js").SOEInputStream;
+exports.SOEOutputStream = require("./out/servers/SoeServer/soeoutputstream.js").SOEOutputStream;
 
-exports.SOEProtocol = require("./out/src/protocols/soeprotocol.js").SOEProtocol;
-exports.LoginProtocol = require("./out/src/protocols/loginprotocol.js").LoginProtocol;
-exports.GatewayProtocol = require("./out/src/protocols/gatewayprotocol.js").GatewayProtocol;
-exports.ZoneProtocol = require("./out/src/protocols/archived/zoneprotocol.js").ZoneProtocol;
-exports.H1Z1Protocol = require("./out/src/protocols/h1z1protocol.js").H1Z1Protocol;
+exports.SOEProtocol = require("./out/protocols/soeprotocol.js").SOEProtocol;
+exports.LoginProtocol = require("./out/protocols/loginprotocol.js").LoginProtocol;
+exports.GatewayProtocol = require("./out/protocols/gatewayprotocol.js").GatewayProtocol;
+exports.ZoneProtocol = require("./out/protocols/archived/zoneprotocol.js").ZoneProtocol;
+exports.H1Z1Protocol = require("./out/protocols/h1z1protocol.js").H1Z1Protocol;
 
-exports.SOEClient = require("./out/src/clients/soeclient.js").SOEClient;
-exports.LoginClient = require("./out/src/clients/loginclient.js").LoginClient;
-exports.GatewayClient = require("./out/src/clients/gatewayclient.js").GatewayClient;
-exports.ZoneClient = require("./out/src/clients/zoneclient.js").ZoneClient;
+exports.SOEClient = require("./out/clients/soeclient.js").SOEClient;
+exports.LoginClient = require("./out/clients/loginclient.js").LoginClient;
+exports.GatewayClient = require("./out/clients/gatewayclient.js").GatewayClient;
+exports.ZoneClient = require("./out/clients/zoneclient.js").ZoneClient;
 
-exports.SOEServer = require("./out/src/servers/SoeServer/soeserver.js").SOEServer;
-exports.LoginServer = require("./out/src/servers/LoginServer/loginserver.js").LoginServer;
-exports.GatewayServer = require("./out/src/servers/GatewayServer/gatewayserver.js").GatewayServer;
-exports.ZoneServer = require("./out/src/servers/ZoneServer/zoneserver.js").ZoneServer;
+exports.SOEServer = require("./out/servers/SoeServer/soeserver.js").SOEServer;
+exports.LoginServer = require("./out/servers/LoginServer/loginserver.js").LoginServer;
+exports.GatewayServer = require("./out/servers/GatewayServer/gatewayserver.js").GatewayServer;
+exports.ZoneServer = require("./out/servers/ZoneServer/zoneserver.js").ZoneServer;
 
-exports.SOEProxy = require("./out/src/proxies/soeproxy.js").SOEProxy;
-exports.LoginProxy = require("./out/src/proxies/loginproxy.js").LoginProxy;
-exports.GatewayProxy = require("./out/src/proxies/gatewayproxy.js").GatewayProxy;
-exports.ZoneProxy = require("./out/src/proxies/zoneproxy.js").ZoneProxy;
-exports.TransparentProxy = require("./out/src/proxies/transproxy.js").TransparentProxy;
+exports.SOEProxy = require("./out/proxies/soeproxy.js").SOEProxy;
+exports.LoginProxy = require("./out/proxies/loginproxy.js").LoginProxy;
+exports.GatewayProxy = require("./out/proxies/gatewayproxy.js").GatewayProxy;
+exports.ZoneProxy = require("./out/proxies/zoneproxy.js").ZoneProxy;
+exports.TransparentProxy = require("./out/proxies/transproxy.js").TransparentProxy;
