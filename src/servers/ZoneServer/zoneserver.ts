51a84ea7ab5879e060635758a546d58bf66fedc7
Quentin Gruber <quentingruber@gmail.com>
2021-02-11 02:00:38 +0100

    Create /types

--- src/servers/ZoneServer/zoneserver.ts
+++ src/servers/ZoneServer/zoneserver.ts
@@ -20,109 +20,11 @@ import _ from "lodash";
 import { Int64String, generateGuid } from "../../utils/utils";
 const debug = require("debug")("ZoneServer");
 const weatherTemplate = require("../../../data/weather.json");
+import {Weather,Client} from "../../../types/zoneserver"
 Date.now = () => {
   // force current time
   return 971172000000;
 };
-
-interface Client {
-  gameClient: {
-    currentWeather: Weather;
-  };
-  transientId: number;
-  transientIds: {};
-  character: {
-    characterId: string;
-    name?: string;
-    loadouts?: any;
-    currentLoadoutTab?: number;
-    currentLoadoutId?: number;
-    currentLoadout?: number;
-    guid?: string;
-    inventory?: Array<any>;
-    factionId?: number;
-    spawnLocation?: string;
-    state: {
-      position: number[];
-      rotation: number[];
-      health: number;
-      shield: number;
-    };
-    client: Client;
-  };
-  sessionId: number;
-  address: string;
-  port: number;
-  crcSeed: number;
-  crcLength: number;
-  clientUdpLength: number;
-  serverUdpLength: number;
-  sequences: any;
-  compression: number;
-  useEncryption: boolean;
-  outQueue: any;
-  outOfOrderPackets: any;
-  nextAck: number;
-  lastAck: number;
-  inputStream: () => void;
-  outputStream: () => void;
-  outQueueTimer: () => void;
-  ackTimer: () => void;
-  outOfOrderTimer: () => void;
-}
-
-interface SendZoneDetailsPacket {
-  zoneName: string;
-  zoneType: number;
-  unknownBoolean1: boolean;
-  skyData: Weather;
-  zoneId1: number;
-  zoneId2: number;
-  nameId: number;
-  unknownBoolean7: boolean;
-}
-
-interface Weather {
-  name: string;
-  unknownDword1: number;
-  unknownDword2: number;
-  unknownDword3: number;
-  unknownDword4: number;
-  fogDensity: number;
-  fogGradient: number;
-  fogFloor: number;
-  unknownDword7: number;
-  rain: number;
-  temp: number;
-  skyColor: number;
-  cloudWeight0: number;
-  cloudWeight1: number;
-  cloudWeight2: number;
-  cloudWeight3: number;
-  sunAxisX: number;
-  sunAxisY: number;
-  sunAxisZ: number;
-  unknownDword18: number;
-  unknownDword19: number;
-  unknownDword20: number;
-  wind: number;
-  unknownDword22: number;
-  unknownDword23: number;
-  unknownDword24: number;
-  unknownDword25: number;
-  unknownArray: UnknownArray[];
-}
-
-interface UnknownArray {
-  unknownDword1: number;
-  unknownDword2: number;
-  unknownDword3: number;
-  unknownDword4: number;
-  unknownDword5: number;
-  unknownDword6: number;
-  unknownDword7: number;
-}
-
 export class ZoneServer extends EventEmitter {
   _gatewayServer: any;
   _protocol: any;
