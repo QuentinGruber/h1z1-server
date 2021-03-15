668b8f4b20f0faa050360c1d72300471d368c86a
Quentin Gruber <quentingruber@gmail.com>
2021-03-15 19:22:02 +0100

    rewrite of soeserver

---
+++ src/types/soeserver.ts
@@ -0,0 +1,22 @@
+export interface Client {
+  sessionId: number;
+  address: string;
+  port: number;
+  crcSeed: number;
+  crcLength: number;
+  clientUdpLength: number;
+  serverUdpLength: number;
+  protocolName: string;
+  sequences: any;
+  compression: number;
+  useEncryption: boolean;
+  outQueue: any;
+  outOfOrderPackets: any;
+  nextAck: number;
+  lastAck: number;
+  inputStream: () => void;
+  outputStream: () => void;
+  outQueueTimer: () => void;
+  ackTimer: () => void;
+  outOfOrderTimer: () => void;
+}
