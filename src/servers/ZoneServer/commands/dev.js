58cc862d2bd257d365567f8c1da1cbddf50853c1
Quentin Gruber <quentingruber@gmail.com>
2021-04-09 00:41:45 +0200

    WIP loot

--- src/servers/ZoneServer/commands/dev.js
+++ src/servers/ZoneServer/commands/dev.js
@@ -17,6 +17,27 @@ const dev = {
       array18: [{ unknown1: 0 }],
     });
   },
+  testLoot: function (server, client, args) {
+    server.sendData(client, "Loot.Reply", {
+      array4: [{ unknown10: 1 }, { unknown10: 1 }],
+    });
+    /*for (let index = 1000; index < 2000; index++) {
+      server.sendData(client, "Loot.Reply", {
+        unknown1: index,
+        unknown2: index,
+        unknown3: index,
+        unknown4: index,
+        unknown5: index,
+        unknown6: index,
+        unknown7: index,
+        unknown8: index,
+        unknown9: index,
+        unknown11: index,
+        array3: [{ unknown1: index }, { unknown1: index }],
+        array4: [{ unknown10: 1 }, { unknown10: 1 }],
+      });
+    }*/
+  },
   reloadPackets: function (server, client, args) {
     if (args[1]) {
       server.reloadPackets(client, args[1]);
