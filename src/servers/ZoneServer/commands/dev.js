5a7c0f4fd0966a725fccb79a46602435417b67f8
Quentin Gruber <quentingruber@gmail.com>
2021-04-17 15:48:05 +0200

    WIP testVehicle ( and a secret command :eyes:  )

--- src/servers/ZoneServer/commands/dev.js
+++ src/servers/ZoneServer/commands/dev.js
@@ -17,26 +17,67 @@ const dev = {
       array18: [{ unknown1: 0 }],
     });
   },
-  testLoot: function (server, client, args) {
-    server.sendData(client, "Loot.Reply", {
-      array4: [{ unknown10: 1 }, { unknown10: 1 }],
-    });
-    /*for (let index = 1000; index < 2000; index++) {
-      server.sendData(client, "Loot.Reply", {
-        unknown1: index,
-        unknown2: index,
-        unknown3: index,
-        unknown4: index,
-        unknown5: index,
-        unknown6: index,
-        unknown7: index,
-        unknown8: index,
-        unknown9: index,
-        unknown11: index,
-        array3: [{ unknown1: index }, { unknown1: index }],
-        array4: [{ unknown10: 1 }, { unknown10: 1 }],
-      });
-    }*/
+  testVehicle: function (server, client, args) {
+    const vehicleData = {
+      npcData: {
+        guid: generateCharacterId(),
+        transientId: 1,
+        unknownString0: "",
+        nameId: 12,
+        unknownDword2: 0,
+        unknownDword3: 0,
+        unknownByte1: 0,
+        modelId: 7225,
+        scale: [1, 1, 1, 1],
+        unknownString1: "",
+        unknownString2: "",
+        unknownDword5: 0,
+        unknownDword6: 0,
+        position: client.character.state.position,
+        unknownVector1: [0, 0, 0, 1],
+        rotation: [0, 0, 0, 1],
+        unknownDword7: 0,
+        unknownFloat1: 3,
+        unknownString3: "",
+        unknownString4: "",
+        unknownString5: "",
+        vehicleId: 3,
+        unknownDword9: 0,
+        npcDefinitionId: 2,
+        unknownByte2: 2,
+        profileId: 3,
+        unknownBoolean1: false,
+        unknownData1: {
+          unknownByte1: 16,
+          unknownByte2: 9,
+          unknownByte3: 0,
+        },
+        unknownByte6: 0,
+        unknownDword11: 0,
+        unknownGuid1: "0x0000000000000000",
+        unknownData2: {
+          unknownGuid1: "0x0000000000000000",
+        },
+        unknownDword12: 0,
+        unknownDword13: 0,
+        unknownDword14: 0,
+        unknownByte7: 0,
+        unknownArray1: [],
+        array5: [{ unknown1: 0 }],
+        array17: [{ unknown1: 0 }],
+        array18: [{ unknown1: 0 }],
+      },
+      unknownGuid1: generateCharacterId(),
+      unknownDword1: 0,
+      unknownDword2: 0,
+      positionUpdate: server.createPositionUpdate(
+        client.character.state.position,
+        [0, 0, 0, 0]
+      ),
+      unknownString1: "mdr",
+    };
+
+    server.sendData(client, "PlayerUpdate.AddLightweightVehicle", vehicleData);
   },
   reloadPackets: function (server, client, args) {
     if (args[1]) {
--- src/servers/ZoneServer/commands/hax.js
+++ src/servers/ZoneServer/commands/hax.js
@@ -16,6 +16,74 @@ const hax = {
     server.removeForcedTime();
     server.sendChatText(client, "Game time is now based on real time", true);
   },
+  spamOffroader: function (server, client, args) {
+    for (let index = 0; index < 150; index++) {
+      const vehicleData = {
+        npcData: {
+          guid: generateCharacterId(),
+          transientId: 1,
+          unknownString0: "",
+          nameId: 12,
+          unknownDword2: 0,
+          unknownDword3: 0,
+          unknownByte1: 0,
+          modelId: 7225,
+          scale: [1, 1, 1, 1],
+          unknownString1: "",
+          unknownString2: "",
+          unknownDword5: 0,
+          unknownDword6: 0,
+          position: client.character.state.position,
+          unknownVector1: [0, 0, 0, 1],
+          rotation: [0, 0, 0, 1],
+          unknownDword7: 0,
+          unknownFloat1: 3,
+          unknownString3: "",
+          unknownString4: "",
+          unknownString5: "",
+          vehicleId: 3,
+          unknownDword9: 0,
+          npcDefinitionId: 2,
+          unknownByte2: 2,
+          profileId: 3,
+          unknownBoolean1: false,
+          unknownData1: {
+            unknownByte1: 16,
+            unknownByte2: 9,
+            unknownByte3: 0,
+          },
+          unknownByte6: 0,
+          unknownDword11: 0,
+          unknownGuid1: "0x0000000000000000",
+          unknownData2: {
+            unknownGuid1: "0x0000000000000000",
+          },
+          unknownDword12: 0,
+          unknownDword13: 0,
+          unknownDword14: 0,
+          unknownByte7: 0,
+          unknownArray1: [],
+          array5: [{ unknown1: 0 }],
+          array17: [{ unknown1: 0 }],
+          array18: [{ unknown1: 0 }],
+        },
+        unknownGuid1: generateCharacterId(),
+        unknownDword1: 0,
+        unknownDword2: 0,
+        positionUpdate: server.createPositionUpdate(
+          client.character.state.position,
+          [0, 0, 0, 0]
+        ),
+        unknownString1: "mdr",
+      };
+
+      server.sendData(
+        client,
+        "PlayerUpdate.AddLightweightVehicle",
+        vehicleData
+      );
+    }
+  },
   spawnNpcModel: function (server, client, args) {
     const guid = server.generateGuid();
     const transientId = server.getTransientId(client, guid);
