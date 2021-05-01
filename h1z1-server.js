6121253e4ad889476444353418b7a1ef4f49ca5d
Quentin Gruber <quentingruber@gmail.com>
2021-05-01 03:03:05 +0200

    remove proxies

--- h1z1-server.js
+++ h1z1-server.js
@@ -13,7 +13,7 @@
 const PackageSetting = require("./package.json");
 
 console.log(PackageSetting.name + " V" + PackageSetting.version);
-console.log(`Node ${process.version}`)
+console.log(`Node ${process.version}`);
 
 exports.SOEInputStream = require("./out/servers/SoeServer/soeinputstream.js").SOEInputStream;
 exports.SOEOutputStream = require("./out/servers/SoeServer/soeoutputstream.js").SOEOutputStream;
@@ -33,9 +33,3 @@ exports.SOEServer = require("./out/servers/SoeServer/soeserver.js").SOEServer;
 exports.LoginServer = require("./out/servers/LoginServer/loginserver.js").LoginServer;
 exports.GatewayServer = require("./out/servers/GatewayServer/gatewayserver.js").GatewayServer;
 exports.ZoneServer = require("./out/servers/ZoneServer/zoneserver.js").ZoneServer;
-
-exports.SOEProxy = require("./out/proxies/soeproxy.js").SOEProxy;
-exports.LoginProxy = require("./out/proxies/loginproxy.js").LoginProxy;
-exports.GatewayProxy = require("./out/proxies/gatewayproxy.js").GatewayProxy;
-exports.ZoneProxy = require("./out/proxies/zoneproxy.js").ZoneProxy;
-exports.TransparentProxy = require("./out/proxies/transproxy.js").TransparentProxy;
