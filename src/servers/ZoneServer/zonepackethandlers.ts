38c962f971b9bdd00603cf16fdb7615d1c02d7d1
avcio9 <79534197+avcio9@users.noreply.github.com>
2021-06-02 05:05:19 +0200

    Update zonepackethandlers.ts
    Fix lack of interactRequest and PlayerSelect
    Still dont work for vehicles and doors

--- src/servers/ZoneServer/zonepackethandlers.ts
+++ src/servers/ZoneServer/zonepackethandlers.ts
@@ -1286,29 +1286,56 @@ const packetHandlers: any = {
       position: [0, 200, 0, 1],
     });
   },
-  "PlayerUpdate.FullCharacterDataRequest": function (
-    server: ZoneServer,
-    client: Client,
-    packet: any
-  ) {
-    const { data:{guid}} = packet
-    if(server._npcs[guid] || server._objects[guid]){
-      server.sendData(client, "PlayerUpdate.LightweightToFullNpc", {
-      characterId: guid,
-      });
-    }
-    else if(server._characters[guid]){
-      server.sendData(client, "PlayerUpdate.LightweightToFullPc", {
-      characterId: guid,
-      });
-    }
-    else if(server._vehicles[guid]){
-      /*server.sendData(client, "PlayerUpdate.LightweightToFullVehicle", {
-      characterId: guid,
-      });*/
-      debug("LightweightToFullVehicle")
-    }
-  },
+  "PlayerUpdate.FullCharacterDataRequest": function (server, client, packet) {
+        const { data: { guid } } = packet;
+		const transientidnpc = server._npcs[guid];
+		const transientidobj = server._objects[guid];
+		const transientidcha = server._characters[guid];
+		const transientiddoo = server._doors[guid];
+		const transientidveh = server._vehicles[guid];
+        if (server._npcs[guid]) {
+            server.sendData(client, "PlayerUpdate.LightweightToFullNpc", {
+                transient_id: transientidnpc.transientId,
+				unknownDword1: 16777215, // PS2 data (i believe these were used for vehicle)
+				unknownDword2: 13951728,
+				unknownDword3: 1,
+				unknownDword6: 100,
+            });
+        }
+		if (server._objects[guid]) {
+            server.sendData(client, "PlayerUpdate.LightweightToFullNpc", {
+                transient_id: transientidobj.transientId,
+				unknownDword1: 16777215,
+				unknownDword2: 13951728,
+				unknownDword3: 1,
+				unknownDword6: 100,
+            });
+        }
+        else if (server._characters[guid]) {
+            server.sendData(client, "PlayerUpdate.LightweightToFullPc", {
+                transient_id: transientidcha.transientId,
+				unknownDword1: 16777215,
+				unknownDword2: 13951728,
+				unknownDword3: 1,
+				unknownDword6: 100,
+            });
+        }
+		else if (server._doors[guid]) {
+            server.sendData(client, "PlayerUpdate.LightweightToFullPc", {
+                transient_id: transientiddoo.transientId,
+				unknownDword1: 16777215,
+				unknownDword2: 13951728,
+				unknownDword3: 1,
+				unknownDword6: 100,
+            });
+        }
+        else if (server._vehicles[guid]) {
+            /*server.sendData(client, "PlayerUpdate.LightweightToFullVehicle", {
+            characterId: guid,
+            });*/
+            debug("LightweightToFullVehicle");
+        }
+    },
 };
 
 export default packetHandlers;
