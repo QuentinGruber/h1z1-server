bbc0fa64705da339b0e722cd85efe29ef9d1ce04
Quentin Gruber <quentingruber@gmail.com>
2021-06-02 16:12:16 +0000

    fix test path

--- tests/loadTests/zoneClientLoadTest.ts
+++ tests/loadTests/zoneClientLoadTest.ts
@@ -3,12 +3,12 @@ import { Int64String } from "../../out/utils/utils.js";
 import { Base64 } from "js-base64";
 import { Worker } from "worker_threads";
 
-const ZoneServer = new Worker(`${__dirname}/workers/ZoneServer.js`);
+const ZoneServer = new Worker(`${__dirname}/workers/zoneServer.js`);
 ZoneServer.on("message", testLoad);
 
 function testLoad() {
   for (let index = 0; index < 100; index++) {
-    var client = new ZoneClient(
+    const client = new ZoneClient(
       "127.0.0.1",
       1117,
       Base64.toUint8Array("F70IaxuU8C/w7FPXY1ibXw=="),
