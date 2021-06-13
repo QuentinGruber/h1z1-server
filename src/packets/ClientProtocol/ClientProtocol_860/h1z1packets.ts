5c33869f5f790dd581b13e9472b9ca49956988bd
avcio9 <79534197+avcio9@users.noreply.github.com>
2021-06-13 04:32:17 +0200

    Update h1z1packets.ts

--- src/packets/ClientProtocol/ClientProtocol_860/h1z1packets.ts
+++ src/packets/ClientProtocol/ClientProtocol_860/h1z1packets.ts
@@ -8202,8 +8202,8 @@ var packets = [
             2: [
               // SetCharacterResource
               { name: "characterId", type: "uint64", defaultValue: "0" },
-              { name: "unknownDword1", type: "uint32", defaultValue: 0 },
-              { name: "unknownDword2", type: "uint32", defaultValue: 0 },
+              { name: "resourceId", type: "uint32", defaultValue: 0 },
+              { name: "resourceType", type: "uint32", defaultValue: 0 },
               {
                 name: "unknownArray1",
                 type: "array",
@@ -8215,7 +8215,7 @@ var packets = [
                   { name: "unknownDword4", type: "float", defaultValue: 0.0 },
                 ],
               },
-              { name: "unknownDword3", type: "uint32", defaultValue: 0 },
+              { name: "initialValue", type: "uint32", defaultValue: 0 },
               { name: "unknownDword4", type: "uint32", defaultValue: 0 },
               { name: "unknownFloat5", type: "float", defaultValue: 0.0 },
               { name: "unknownFloat6", type: "float", defaultValue: 0.0 },
