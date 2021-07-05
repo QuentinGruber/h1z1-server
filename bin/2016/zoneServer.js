f0b715f26286853e31f5c9d145aeec23d00c396e
Quentin Gruber <quentingruber@gmail.com>
2021-07-06 01:01:05 +0200

    fix ts to js error

--- bin/2016/zoneServer.js
+++ bin/2016/zoneServer.js
@@ -2,6 +2,6 @@ const { ZoneServer2016 } = require("../../h1z1-server");
 
 const Zone = new ZoneServer2016(
   1117,
-  new (Buffer as any).from("F70IaxuU8C/w7FPXY1ibXw==", 'base64')
+  new Buffer.from("F70IaxuU8C/w7FPXY1ibXw==", "base64")
 );
 Zone.start();
