de44580ae6e58c5c8fa205b626d410d203de6794
Quentin Gruber <quentingruber@gmail.com>
2021-07-09 18:32:41 +0200

    setup env vars

--- docker/2015/loginServer.js
+++ docker/2015/loginServer.js
@@ -1,3 +1,3 @@
 const { LoginServer } = require("../../h1z1-server");
-const server = new LoginServer(1115, "");
+const server = new LoginServer(1115, process.env.MONGO_URL);
 server.start();
--- docker/2016/loginServer.js
+++ docker/2016/loginServer.js
@@ -1,4 +1,4 @@
 const { LoginServer, LoginProtocol } = require("../../h1z1-server");
-const server = new LoginServer(1115, "");
+const server = new LoginServer(1115, process.env.MONGO_URL);
 server._protocol = new LoginProtocol("LoginUdp_11");
 server.start();
