c77e9fb8fba89a4b47b6fefc0bd000b0ac8ca1d8
Quentin Gruber <quentingruber@gmail.com>
2021-07-09 14:05:19 +0200

    prettier /docker

--- docker/2016/h1emuServer.js
+++ docker/2016/h1emuServer.js
@@ -1,34 +1,32 @@
+const { spawn } = require("child_process");
 
-const { spawn } = require('child_process');
+const loginServer = spawn("node", ["./loginServer.js"]);
+const zoneServer = spawn("node", ["./zoneServer.js"]);
 
-const loginServer = spawn('node', ['./loginServer.js']);
-const zoneServer = spawn('node', ['./zoneServer.js']);
+loginServer.stdout.on("data", (data) => {
+  console.log(`${data}`);
+});
 
+loginServer.stderr.on("data", (data) => {
+  console.log(`${data}`);
+});
 
-loginServer.stdout.on('data', (data) => {
-        console.log(`${data}`);
-      });
-      
-      loginServer.stderr.on('data', (data) => {
-        console.log(`${data}`);
-      });
-      
-      loginServer.on('close', (code) => {
-          if(code){  
-            throw new Error((`${name}(${version}) exited with code ${code}`));
-          }
-      });
+loginServer.on("close", (code) => {
+  if (code) {
+    throw new Error(`${name}(${version}) exited with code ${code}`);
+  }
+});
 
-      zoneServer.stdout.on('data', (data) => {
-        console.log(`${data}`);
-      });
-      
-      zoneServer.stderr.on('data', (data) => {
-        console.log(`${data}`);
-      });
-      
-      zoneServer.on('close', (code) => {
-          if(code){  
-            throw new Error((`${name}(${version}) exited with code ${code}`));
-          }
-      });
+zoneServer.stdout.on("data", (data) => {
+  console.log(`${data}`);
+});
+
+zoneServer.stderr.on("data", (data) => {
+  console.log(`${data}`);
+});
+
+zoneServer.on("close", (code) => {
+  if (code) {
+    throw new Error(`${name}(${version}) exited with code ${code}`);
+  }
+});
--- docker/buildDocker.ts
+++ docker/buildDocker.ts
@@ -1,37 +1,40 @@
-import { spawn } from 'child_process';
-const images:ServerImage[] = require("./images.json")
+import { spawn } from "child_process";
+const images: ServerImage[] = require("./images.json");
 
 interface ServerImage {
-    name:string;
-    version:string;
-    fileName:string;
+  name: string;
+  version: string;
+  fileName: string;
 }
 
+function buildAllImages() {
+  images.forEach((image) => {
+    const { name, version, fileName } = image;
+    const buildProcess = spawn("docker", [
+      "build",
+      "-t",
+      `h1emu/${name}`,
+      "-f",
+      `./docker/${version}/${fileName}.Dockerfile`,
+      "./",
+    ]);
 
-  function buildAllImages() {
-
-    images.forEach(image => {
-        const {name , version, fileName}= image;
-        const buildProcess = spawn('docker', ['build', '-t', `h1emu/${name}`, '-f', `./docker/${version}/${fileName}.Dockerfile`, './']);
-    
-    buildProcess.stdout.on('data', (data) => {
-        console.log(`${name}(${version}): ${data}`);
-      });
-      
-      buildProcess.stderr.on('data', (data) => {
-        console.log(`${name}(${version}): ${data}`);
-      });
-      
-      buildProcess.on('close', (code) => {
-          if(code){  
-            throw new Error((`${name}(${version}) exited with code ${code}`));
-          }
-          else{
-            console.log(`${name}(${version}) builded!`);
-          }
-      });
+    buildProcess.stdout.on("data", (data) => {
+      console.log(`${name}(${version}): ${data}`);
     });
-    
-  }
 
-  buildAllImages()
+    buildProcess.stderr.on("data", (data) => {
+      console.log(`${name}(${version}): ${data}`);
+    });
+
+    buildProcess.on("close", (code) => {
+      if (code) {
+        throw new Error(`${name}(${version}) exited with code ${code}`);
+      } else {
+        console.log(`${name}(${version}) builded!`);
+      }
+    });
+  });
+}
+
+buildAllImages();
