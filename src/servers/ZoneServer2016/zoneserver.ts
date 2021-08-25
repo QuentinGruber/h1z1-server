1e7dce96c1d71997e116590b55d2174c1bf02e65
Cory M <csm4502@gmail.com>
2021-08-25 14:59:47 -0400

    removed setupCharacter() function, since characterId is now set through the zoneclient constructor

--- src/servers/ZoneServer2016/zoneserver.ts
+++ src/servers/ZoneServer2016/zoneserver.ts
@@ -319,19 +319,6 @@ export class ZoneServer2016 extends ZoneServer {
     this.worldRoutineTimer = setTimeout(()=>this.worldRoutine2016.bind(this)(true), 3000);
   }
 
-
-  setupCharacter(client: Client, characterId: string) {
-    // only sets characterId for character selected from login server (probably not needed for 2016)
-  let generatedTransient;
-    do {
-      generatedTransient = Number((Math.random() * 30000).toFixed(0));
-    } while (this._transientIds[generatedTransient]);
-    client.character = {
-      ...client.character,
-      characterId: characterId,
-    }
-  }
-
   sendInitData(client: Client): void {
     this.sendData(client, "InitializationParameters", {
       environment: "LIVE",
