626b6ace165abd4cc2efbe11bd42557aa6bba65e
Quentin Gruber <quentingruber@gmail.com>
2021-08-31 00:17:28 +0200

    Update zoneClientTest.ts

--- tests/2015/mongo/zoneClientTest.ts
+++ tests/2015/mongo/zoneClientTest.ts
@@ -8,25 +8,27 @@ async function test() {
     1
   ).start();
 
-  var client = new ZoneClient(
-    "127.0.0.1",
-    1117,
-    new (Buffer as any).from("F70IaxuU8C/w7FPXY1ibXw==", "base64"),
-    "0x03147cca2a860195",
-    "0",
-    "",
-    "",
-    6457
-  );
-  client.connect();
-  client.on("connect", (err, res) => {
-    console.log("connect");
-  });
-  client.on("ZoneDoneSendingInitialData", (err, res) => {
-    console.log("ZoneDoneSendingInitialData");
-    process.exit(0);
-  });
-
+  setTimeout(() => {
+    var client = new ZoneClient(
+      "127.0.0.1",
+      1117,
+      new (Buffer as any).from("F70IaxuU8C/w7FPXY1ibXw==", "base64"),
+      "0x03147cca2a860195",
+      "0",
+      "",
+      "",
+      6457
+    );
+    client.connect();
+    client.on("connect", (err, res) => {
+      console.log("connect");
+    });
+    client.on("ZoneDoneSendingInitialData", (err, res) => {
+      console.log("ZoneDoneSendingInitialData");
+      process.exit(0);
+    });
+  }, 2000);
+  
   setTimeout(() => {
     throw new Error("Test timed out!");
   }, 60000);
