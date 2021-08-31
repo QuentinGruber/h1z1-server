79b88576a1e8f0031d767fab5fd0c69556203127
Quentin Gruber <quentingruber@gmail.com>
2021-08-31 20:35:30 +0200

    Add mongodb debug script for vscode

--- src/servers/LoginServer/loginserver.ts
+++ src/servers/LoginServer/loginserver.ts
@@ -544,11 +544,11 @@ if (process.env.VSCODE_DEBUG === "true") {
   if (process.env.CLIENT_SIXTEEN === "true") {
     const server = new LoginServer(
       1115, // <- server port
-      "" // <- MongoDB address ( if blank server start in solo mode )
+      process.env.MONGO_URL // <- MongoDB address ( if blank server start in solo mode )
     );
     server._protocol = new LoginProtocol("LoginUdp_11");
     server.start();
   } else {
-    new LoginServer(1115, "").start();
+    new LoginServer(1115, process.env.MONGO_URL).start();
   }
 }
--- src/servers/ZoneServer/zoneserver.ts
+++ src/servers/ZoneServer/zoneserver.ts
@@ -1258,6 +1258,7 @@ if (
 ) {
   new ZoneServer(
     1117,
-    new (Buffer as any).from("F70IaxuU8C/w7FPXY1ibXw==", "base64")
+    new (Buffer as any).from("F70IaxuU8C/w7FPXY1ibXw==", "base64"),
+    process.env.MONGO_URL
   ).start();
 }
