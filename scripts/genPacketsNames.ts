9ba6ebc5ec51090d126d2bc5a31d7f8fdc875eef
Quentin Gruber <quentingruber@gmail.com>
2021-09-02 23:10:52 +0200

    setup the use of h1z1PacketsType

--- scripts/genPacketsNames.ts
+++ scripts/genPacketsNames.ts
@@ -6,20 +6,19 @@ const h1z1packets2015 = require("../src/packets/ClientProtocol/ClientProtocol_86
 const h1z1packets2016 = require("../src/packets/ClientProtocol/ClientProtocol_1080/h1z1packets")
 
 const packetsFiles:any = {
-    h1z1Packets2015:Object.keys(h1z1packets2015.PacketTypes),
-    h1z1Packets2016:Object.keys(h1z1packets2016.PacketTypes),
-    loginPackets2015:Object.keys(loginpackets2015.PacketTypes),
-    loginPackets2016:Object.keys(loginpackets2016.PacketTypes),
+    h1z1PacketsType:[...Object.keys(h1z1packets2015.PacketTypes),...Object.keys(h1z1packets2016.PacketTypes)],
+    loginPackets2015Type:Object.keys(loginpackets2015.PacketTypes),
+    loginPackets2016Type:Object.keys(loginpackets2016.PacketTypes),
 };
 
 const typePath:string = "../src/types/packets.ts"
-let steamString ="";
+let steamString =""; // should use a streamWriter but i'm lazy
 Object.keys(packetsFiles).forEach(packetsFileName => {
     const packetsFile:string[] = packetsFiles[packetsFileName];
     steamString += `export type ${packetsFileName} = `
     for (let index = 0; index < packetsFile.length; index++) {
         const packet:string = packetsFile[index];
-        steamString += `"${packet}" | `;
+        steamString += ` | "${packet}"`;
     }
     steamString += `; \n`;
 });
