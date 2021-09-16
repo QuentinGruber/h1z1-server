9defa8d057d98b6100e8a1e5f379e720fc480b4a
Quentin Gruber <quentingruber@gmail.com>
2021-09-16 22:02:29 +0200

    :clown:

--- scripts/h1z1-server-demo-2016.js
+++ scripts/h1z1-server-demo-2016.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 const { Worker } = require("worker_threads");
 new Worker(`${__dirname}/demo/loginserver2016.js`)
-new Worker(`${__dirname}./demo/ZoneServer2016.js`)
+new Worker(`${__dirname}/demo/ZoneServer2016.js`)
