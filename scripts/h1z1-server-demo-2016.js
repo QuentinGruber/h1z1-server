bcdc92a11a13053238fa876ebca570bf36ee83e0
Quentin Gruber <quentingruber@gmail.com>
2021-09-16 21:58:49 +0200

    fix path issues

--- scripts/h1z1-server-demo-2016.js
+++ scripts/h1z1-server-demo-2016.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 const { Worker } = require("worker_threads");
-new Worker("./demo/loginserver2016.js")
-new Worker("./demo/ZoneServer2016.js")
+new Worker(`${__dirname}/demo/loginserver2016.js`)
+new Worker(`${__dirname}./demo/ZoneServer2016.js`)
--- scripts/h1z1-server-demo.js
+++ scripts/h1z1-server-demo.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 const { Worker } = require("worker_threads");
-new Worker("./demo/loginserver2015.js")
-new Worker("./demo/ZoneServer2015.js")
+new Worker(`${__dirname}/demo/loginserver2015.js`)
+new Worker(`${__dirname}/demo/ZoneServer2015.js`)
