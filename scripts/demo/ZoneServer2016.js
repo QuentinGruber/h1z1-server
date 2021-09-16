99b1d9dc694238d0cb67d1208ae7d7db3b5782c6
Quentin Gruber <quentingruber@gmail.com>
2021-09-16 22:10:05 +0200

    fix base64 old forgotten dependency

--- scripts/demo/ZoneServer2016.js
+++ scripts/demo/ZoneServer2016.js
@@ -1,8 +1,7 @@
 process.env.DEBUG = "ZoneServer";
 const H1Z1servers = require("../../h1z1-server");
-const { Base64 } = require("js-base64");
 var Zone = new H1Z1servers.ZoneServer2016(
   1117,
-  Base64.toUint8Array("F70IaxuU8C/w7FPXY1ibXw==")
+  new Buffer.from("F70IaxuU8C/w7FPXY1ibXw==", "base64")
 );
 Zone.start();
