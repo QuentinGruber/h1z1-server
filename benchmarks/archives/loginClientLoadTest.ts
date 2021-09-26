da185ee2a03da4ce5175e6012ddcc11dab2a72c4
Quentin Gruber <quentingruber@gmail.com>
2021-09-26 16:10:26 +0200

    prettier benchmarks

--- benchmarks/archives/loginClientLoadTest.ts
+++ benchmarks/archives/loginClientLoadTest.ts
@@ -2,40 +2,40 @@ import { LoginClient } from "../../h1z1-server";
 
 import { Worker } from "worker_threads";
 const loginServer = new Worker(`${__dirname}/workers/loginServer.js`);
-loginServer.on("message", ()=>setTimeout(testLoad,100));
+loginServer.on("message", () => setTimeout(testLoad, 100));
 
 function testLoad() {
-    for (let index = 0; index < 100; index++) {
-      const client = new LoginClient(
-        295110,
-        "dev",
-        "127.0.0.1",
-        1115,
-        Buffer.from("F70IaxuU8C/w7FPXY1ibXw==", "base64"), // <- loginkey
-        4851 + index
-      );
-      console.time("FullLogin" + index);
-      queueMicrotask(()=>client.connect())
-      client.on("login", (err, res) => {
-        if (res.loggedIn) {
-          client.requestServerList();
-        }
+  for (let index = 0; index < 100; index++) {
+    const client = new LoginClient(
+      295110,
+      "dev",
+      "127.0.0.1",
+      1115,
+      Buffer.from("F70IaxuU8C/w7FPXY1ibXw==", "base64"), // <- loginkey
+      4851 + index
+    );
+    console.time("FullLogin" + index);
+    queueMicrotask(() => client.connect());
+    client.on("login", (err, res) => {
+      if (res.loggedIn) {
+        client.requestServerList();
+      }
+    });
+    client.on("serverlist", (err, res) => {
+      client.requestCharacterInfo();
+    });
+    client.on("charactercreate", (err, res) => {
+      client.requestCharacterLogin(res.characterId, 1, {
+        locale: "EnUS",
+        localeId: 1,
+        preferredGatewayId: 8,
       });
-      client.on("serverlist", (err, res) => {
-        client.requestCharacterInfo();
-      });
-      client.on("charactercreate", (err, res) => {
-          client.requestCharacterLogin(res.characterId, 1, {
-            locale: "EnUS",
-            localeId: 1,
-            preferredGatewayId: 8,
-          });
-      });
-      client.on("characterinfo", (err, res) => {
-        client.requestCharacterCreate();
-      });
-      client.on("characterlogin", (err, res) => {
-        console.timeEnd("FullLogin" + index);
-      });
-    }
+    });
+    client.on("characterinfo", (err, res) => {
+      client.requestCharacterCreate();
+    });
+    client.on("characterlogin", (err, res) => {
+      console.timeEnd("FullLogin" + index);
+    });
+  }
 }
--- benchmarks/workers/soeServer.ts
+++ benchmarks/workers/soeServer.ts
@@ -2,7 +2,13 @@ import { parentPort } from "worker_threads";
 import { SOEServer } from "../../h1z1-server";
 
 async function setup() {
-  new SOEServer("test",1115,Buffer.from("F70IaxuU8C/w7FPXY1ibXw==", "base64"),0,false).start(0,0,2,512);
+  new SOEServer(
+    "test",
+    1115,
+    Buffer.from("F70IaxuU8C/w7FPXY1ibXw==", "base64"),
+    0,
+    false
+  ).start(0, 0, 2, 512);
   parentPort.postMessage("started");
 }
 
