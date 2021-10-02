052c1469a5f6442b2a288280a2745f034d45b8a9
Quentin Gruber <quentingruber@gmail.com>
2021-10-02 16:26:10 +0200

    use eul2quat from utils

--- src/packets/ClientProtocol/ClientProtocol_1080/h1z1packets.ts
+++ src/packets/ClientProtocol/ClientProtocol_1080/h1z1packets.ts
@@ -12,8 +12,7 @@
 // ======================================================================
 import PacketTableBuild from "../../packettable";
 import DataSchema from "h1z1-dataschema";
-import { lz4_decompress } from "../../../utils/utils";
-import eul2quat from "eul2quat";
+import { lz4_decompress , eul2quat} from "../../../utils/utils";
 
 function readPacketType(data: Buffer, packets: any) {
   let opCode = data[0] >>> 0,
