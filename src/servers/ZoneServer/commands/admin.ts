d7c6f06ed9358a1f67e26e6859df698c6592bbb3
Quentin Gruber <quentingruber@gmail.com>
2021-11-14 02:58:14 +0100

    await more promises

--- src/servers/ZoneServer/commands/admin.ts
+++ src/servers/ZoneServer/commands/admin.ts
@@ -14,7 +14,7 @@ const admin: any = {
     const timeLeft = args[1] ? args[1] : 0;
     const message = args[2] ? args[2] : " ";
     const startedTime = Date.now();
-    zoneShutdown(server, startedTime, timeLeft, message);
+    await zoneShutdown(server, startedTime, timeLeft, message);
   },
 };
 
