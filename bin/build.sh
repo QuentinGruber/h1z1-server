3986f8dd6d81c8eccfb3a61a7ecea3c1e4ecfb14
Quentin Gruber <quentingruber@gmail.com>
2021-11-16 11:17:21 +0000

    fix: :bug: build-binaries-unix

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,6 @@
-RUN pkg -t node16-win-x64 ./2015/loginServer.js --output ./loginServer.exe
-RUN pkg -t node16-win-x64 ./2015/zoneServer.js --output ./zoneServer.exe
-RUN pkg -t node16-win-x64 ./2015/h1emuServer.js --output ./h1emuServer.exe
-RUN pkg -t node16-win-x64 ./2016/loginServer.js --output ./loginServer-2016.exe
-RUN pkg -t node16-win-x64 ./2016/zoneServer.js --output ./zoneServer-2016.exe
-RUN pkg -t node16-win-x64 ./2016/h1emuServer.js --output ./h1emuServer-2016.exe
+pkg -t node16-win-x64 ./bin/2015/loginServer.js --output ./bin/loginServer.exe
+pkg -t node16-win-x64 ./bin/2015/zoneServer.js --output ./bin/zoneServer.exe
+pkg -t node16-win-x64 ./bin/2015/h1emuServer.js --output ./bin/h1emuServer.exe
+pkg -t node16-win-x64 ./bin/2016/loginServer.js --output ./bin/loginServer-2016.exe
+pkg -t node16-win-x64 ./bin/2016/zoneServer.js --output ./bin/zoneServer-2016.exe
+pkg -t node16-win-x64 ./bin/2016/h1emuServer.js --output ./bin/h1emuServer-2016.exe
