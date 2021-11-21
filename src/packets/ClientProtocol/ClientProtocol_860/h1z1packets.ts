2bdb15fa84112b0ea7c7a865c0fcbc5316d47ffb
Haxmax <63957530+thegrreat1@users.noreply.github.com>
2021-11-21 15:27:16 +0100

    Reversed skillpackets

--- src/packets/ClientProtocol/ClientProtocol_860/h1z1packets.ts
+++ src/packets/ClientProtocol/ClientProtocol_860/h1z1packets.ts
@@ -8139,12 +8139,42 @@ var packets = [
   ["Facility.EmpireScoreValueUpdate", 0x8417, {}],
   ["Skill.Echo", 0x8501, {}],
   ["Skill.SelectSkillSet", 0x8502, {}],
-  ["Skill.SelectSkill", 0x8503, {}],
+  ["Skill.SelectSkill", 0x8503,
+          {
+            fields: [
+                { name: "unknownDword1", type: "uint32", defaultValue: 0 },
+                { name: "unknownWord1", type: "uint8", defaultValue: 0 },
+                { name: "unknownDword2", type: "uint32", defaultValue: 0 },
+          ]
+      },
+  ],
   ["Skill.GetSkillPointManager", 0x8504, {}],
   ["Skill.SetLoyaltyPoints", 0x8505, {}],
   ["Skill.LoadSkillDefinitionManager", 0x8506, {}],
-  ["Skill.SetSkillPointManager", 0x8507, {}],
-  [
+  ["Skill.SetSkillPointManager",
+        0x8507,
+        {
+            fields: [
+                { name: "unknownDword1", type: "uint32", defaultValue: 0 },
+
+                {
+                    name: "unknownSchema1",
+                    type: "schema",
+                    defaultValue: [],
+                    fields: [
+                        { name: "unknownQword1", type: "uint64string", defaultValue: "0x000" },
+                        { name: "unknownQword2", type: "uint64string", defaultValue: "0x000" },
+                        { name: "unknownQword3", type: "uint64string", defaultValue: "0x000" },
+                        { name: "unknownQword4", type: "uint64string", defaultValue: "0x000" },
+                        { name: "unknownQword5", type: "uint64string", defaultValue: "0x000" },
+                        { name: "unknownDword2", type: "uint32", defaultValue: 0 },
+
+                      ],
+                  },
+              ],
+          },
+      ],
+    [
     "Skill.SetSkillPointProgress",
     0x8508,
     {
