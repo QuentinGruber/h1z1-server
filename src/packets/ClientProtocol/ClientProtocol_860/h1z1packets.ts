7601526af9a6fe2698338aec368448d1b4a6bf61
Haxmax <63957530+thegrreat1@users.noreply.github.com>
2021-11-21 02:02:26 +0100

    Updated packets

--- src/packets/ClientProtocol/ClientProtocol_860/h1z1packets.ts
+++ src/packets/ClientProtocol/ClientProtocol_860/h1z1packets.ts
@@ -7154,6 +7154,34 @@ var packets = [
   ["Report", 0x29, {}],
   ["LiveGamer", 0x2a, {}],
   ["Acquaintance", 0x2b, {}],
+  ["AcquaintanceAdd",
+    0x2b01,
+    {
+        fields: [
+            { name: "characterId", type: "uint64string", defaultValue: "0x000" },
+            { name: "characterName", type: "string", defaultValue: "0" },
+            { name: "type", type: "uint32", defaultValue: 0 },
+            { name: "elapsedTime", type: "uint64string", defaultValue: "0x000" },
+            { name: "isOnline", type: "uint8", defaultValue: 0 },
+          ],
+       },
+    ],
+    ["AcquaintanceRemove",
+    0x2b02,
+    {
+        fields: [
+          ],
+       },
+    ],
+    ["AcquaintanceOnline",
+    0x2b03,
+    {
+        fields: [
+            { name: "characterId", type: "uint64string", defaultValue: "0x000" },
+            { name: "isOnline", type: "boolean", defaultValue: 0 },
+        ],
+     },
+  ],
   ["ClientServerShuttingDown", 0x2c, {}],
   [
     "Friend.List",
