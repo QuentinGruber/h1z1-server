67465d00c532a29d685e3db0fce1955b60f73c3b
Haxmax <63957530+thegrreat1@users.noreply.github.com>
2021-11-21 16:21:46 +0100

    Added initability
    Reversed using homemade tool. No failflags and it's getting executed so seems like the correct struct?

--- src/packets/ClientProtocol/ClientProtocol_860/h1z1packets.ts
+++ src/packets/ClientProtocol/ClientProtocol_860/h1z1packets.ts
@@ -9465,7 +9465,83 @@ var packets = [
   ["RewardBuffs.AddBonus", 0x9f03, {}],
   ["RewardBuffs.RemoveBonus", 0x9f04, {}],
   ["RewardBuffs.GiveRewardToPlayer", 0x9f05, {}],
-  ["Abilities.InitAbility", 0xa001, {}],
+  ["Abilities.InitAbility",
+        0xa001,
+        {
+            fields: [
+                { name: "unknownDword1", type: "uint32", defaultValue: 0 },
+                { name: "unknownDword2", type: "uint32", defaultValue: 0 },
+                { name: "unknownDword3", type: "uint32", defaultValue: 0 },
+                { name: "unknownDword4", type: "uint32", defaultValue: 0 },
+                { name: "unknownQword1", type: "uint64string", defaultValue: "0x000" },
+                { name: "unknownQword2", type: "uint64string", defaultValue: "0x000" },
+                {
+                    name: "unknownSchema1",
+                    type: "schema",
+                    defaultValue: [],
+                    fields: [
+                        { name: "unknownQword3", type: "uint64string", defaultValue: "0x000" },
+                        { name: "unknownQword4", type: "uint64string", defaultValue: "0x000" },
+                        { name: "unknownFloatVector1", type: "floatvector4", defaultValue: 0 },
+                    ],
+                },
+                { name: "unknownWord1", type: "uint8", defaultValue: 0 },
+                {
+                    name: "unknownArray6",
+                    type: "schema",
+                    defaultValue: [],
+                    fields: [
+                        { name: "unknownWord2", type: "uint8", defaultValue: 0 },
+                        {
+                            name: "unknownArray1",
+                            type: "array",
+                            defaultValue: [],
+                            fields: [
+                                { name: "unknownDword6", type: "uint32", defaultValue: 0 },
+                                { name: "unknownDword7", type: "uint32", defaultValue: 0 },
+                            ],
+                        },
+                        {
+                            name: "unknownArray2",
+                            type: "array",
+                            defaultValue: [],
+                            fields: [
+                                { name: "unknownDword8", type: "uint32", defaultValue: 0 },
+                                { name: "unknownDword9", type: "uint32", defaultValue: 0 },
+                            ],
+                        },
+                        {
+                            name: "unknownArray3",
+                            type: "array",
+                            defaultValue: [],
+                            fields: [
+                                { name: "unknownDword10", type: "uint32", defaultValue: 0 },
+                                { name: "unknownWord1", type: "uint8", defaultValue: 0 },
+                            ],
+                        },
+                        {
+                            name: "unknownArray4",
+                            type: "array",
+                            defaultValue: [],
+                            fields: [
+                                { name: "unknownDword11", type: "uint32", defaultValue: 0 },
+                                { name: "unknownFloatVector2", type: "floatvector4", defaultValue: 0 },
+                            ],
+                        },
+                        {
+                            name: "unknownArray5",
+                            type: "array",
+                            defaultValue: [],
+                            fields: [
+                                { name: "unknownDword13", type: "uint32", defaultValue: 0 },
+                                { name: "unknownString1", type: "string", defaultValue: "0" },
+                          ],
+                      },
+                  ],
+              },
+          ],
+      },
+  ],
   ["Abilities.UpdateAbility", 0xa002, {}],
   ["Abilities.UninitAbility", 0xa003, {}],
   ["Abilities.SetAbilityActivationManager", 0xa004, {}],
