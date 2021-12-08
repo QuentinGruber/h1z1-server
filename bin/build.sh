7240db06f4193f016c067df02c4298599a1e847d
QuentinGruber <QuentinGruber@gmail.com>
2021-12-08 16:56:41 +0100

    feat: :zap: use Gzip compression for binaries ( 70% smaller )

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,6 @@
-pkg -t node16-win-x64 ./bin/2015/loginServer.js --output ./bin/loginServer.exe
-pkg -t node16-win-x64 ./bin/2015/zoneServer.js --output ./bin/zoneServer.exe
-pkg -t node16-win-x64 ./bin/2015/h1emuServer.js --output ./bin/h1emuServer.exe
-pkg -t node16-win-x64 ./bin/2016/loginServer.js --output ./bin/loginServer-2016.exe
-pkg -t node16-win-x64 ./bin/2016/zoneServer.js --output ./bin/zoneServer-2016.exe
-pkg -t node16-win-x64 ./bin/2016/h1emuServer.js --output ./bin/h1emuServer-2016.exe
+pkg -t node16-win-x64 --compress GZip ./bin/2015/loginServer.js --output ./bin/loginServer.exe
+pkg -t node16-win-x64 --compress GZip ./bin/2015/zoneServer.js --output ./bin/zoneServer.exe
+pkg -t node16-win-x64 --compress GZip ./bin/2015/h1emuServer.js --output ./bin/h1emuServer.exe
+pkg -t node16-win-x64 --compress GZip ./bin/2016/loginServer.js --output ./bin/loginServer-2016.exe
+pkg -t node16-win-x64 --compress GZip ./bin/2016/zoneServer.js --output ./bin/zoneServer-2016.exe
+pkg -t node16-win-x64 --compress GZip ./bin/2016/h1emuServer.js --output ./bin/h1emuServer-2016.exe
