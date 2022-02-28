712338a75caac9b51adecbda0904415442e99305
avcio9 <79534197+avcio9@users.noreply.github.com>
2022-02-28 06:49:21 +0100

    add Command.PlayDialogEffect structure

--- src/packets/ClientProtocol/ClientProtocol_1080/command.ts
+++ src/packets/ClientProtocol/ClientProtocol_1080/command.ts
@@ -144,7 +144,16 @@ export const commandPackets: any = [
   ],
   ["Command.ShowRecipeWindow", 0x091b00, {}],
   ["Command.ActivateProfileFailed", 0x091c00, {}],
-  ["Command.PlayDialogEffect", 0x091d00, {}],
+  [
+    "Command.PlayDialogEffect",
+    0x091d00,
+    {
+      fields: [
+        { name: "characterId", type: "uint64string", defaultValue: "0x000" },
+        { name: "effectId", type: "uint32", defaultValue: 0 },
+      ],
+    },
+  ],
   ["Command.ForceClearDialog", 0x091e00, {}],
   ["Command.IgnoreRequest", 0x091f00, {}],
   ["Command.SetActiveVehicleGuid", 0x092000, {}],
