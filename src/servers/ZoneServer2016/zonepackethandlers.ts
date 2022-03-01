4ba4e928ccb56bff62cdfdf7288c4c88897afb7a
avcio9 <79534197+avcio9@users.noreply.github.com>
2022-03-01 05:46:35 +0100

    give items on recipeStart packet

--- src/servers/ZoneServer2016/zonepackethandlers.ts
+++ src/servers/ZoneServer2016/zonepackethandlers.ts
@@ -245,7 +245,7 @@ export class zonePacketHandlers {
       packet: any
     ) {
       debug(packet);
-      //server.sendData(client, "Command.RecipeAction", {});
+      server.dummyCraft(client, packet.data.recipeId, packet.data.count);
     };
     this.CommandSetInWater = function (
       server: ZoneServer2016,
