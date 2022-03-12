9bffd92f1afad966506968a7a11072a24f03ed14
Cory M <csm4502@gmail.com>
2022-03-12 00:32:54 -0500

    Added WORLD_MODEL_ID to .308

--- src/packets/ClientProtocol/ClientProtocol_1080/shared.ts
+++ src/packets/ClientProtocol/ClientProtocol_1080/shared.ts
@@ -806,7 +806,7 @@ export const lightWeightNpcSchema = [
   { name: "vehicleId", type: "uint32", defaultValue: 0 },
   { name: "unknownDword5", type: "uint32", defaultValue: 0 },
   { name: "npcDefinitionId", type: "uint32", defaultValue: 0 },
-  { name: "positionUpdateType", type: "boolean", defaultValue: 0 }, // determine if npc is moving with positionUpdate - Avcio
+  { name: "positionUpdateType", type: "boolean", defaultValue: false }, // determine if npc is moving with positionUpdate - Avcio
   { name: "unknownDword7", type: "uint32", defaultValue: 0 },
   { name: "dontSendFullNpcRequest", type: "boolean", defaultValue: false },
   {
