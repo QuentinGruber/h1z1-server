b6db29be3f88cf295b60e07516070d6721fcf817
avcio9 <79534197+avcio9@users.noreply.github.com>
2022-03-21 19:53:41 +0100

    add /hax spamIED command

--- src/servers/ZoneServer2016/commands/hax.ts
+++ src/servers/ZoneServer2016/commands/hax.ts
@@ -302,6 +302,62 @@ const hax: any = {
     server.removeForcedTime();
     server.sendChatText(client, "Game time is now based on real time", true);
   },
+  spamied: function (server: ZoneServer2016, client: Client, args: any[]) {
+    if (!args[2]) {
+      server.sendChatText(
+        client,
+        "[ERROR] Usage /hax spamIED [RANGE] [POINTS]"
+      );
+      return;
+    }
+    const multiplied = Number(args[1]) * Number(args[2]);
+    if (multiplied > 600) {
+      server.sendChatText(
+        client,
+        `[ERROR]Maximum RANGE * POINTS value reached: ("${multiplied}"/600)`
+      );
+      return;
+    }
+    const range = Number(args[1]);
+    const lat = client.character.state.position[0];
+    const long = client.character.state.position[2];
+    let points = [];
+    let rangeFixed = range;
+    let numberOfPoints = Number(args[2]);
+    let degreesPerPoint = 360 / numberOfPoints;
+    for (let j = 1; j < range; j++) {
+      let currentAngle = 0;
+      let x2;
+      let y2;
+      rangeFixed += -1;
+      for (let i = 0; i < numberOfPoints; i++) {
+        x2 = Math.cos(currentAngle) * rangeFixed;
+        y2 = Math.sin(currentAngle) * rangeFixed;
+        const p = [lat + x2, long + y2];
+        points.push(p);
+        currentAngle += degreesPerPoint;
+      }
+    }
+    points.forEach((obj: any) => {
+      const characterId = server.generateGuid();
+      const guid = server.generateGuid();
+      const transientId = server.getTransientId(guid);
+      const npc = {
+        characterId: characterId,
+        guid: guid,
+        transientId: transientId,
+        modelId: 9176,
+        position: [obj[0], client.character.state.position[1], obj[1], 1],
+        rotation: client.character.state.lookAt,
+        dontSendFullNpcRequest: true,
+        color: {},
+        attachedObject: {},
+        isIED: true,
+      };
+
+      server._explosives[characterId] = npc; // save npc
+    });
+  },
   spamatv: function (server: ZoneServer2016, client: Client, args: any[]) {
     for (let index = 0; index < 50; index++) {
       const guid = server.generateGuid();
