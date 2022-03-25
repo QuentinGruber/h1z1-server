d6abd4f447243109315c118b8486266af44c4149
Quentin Gruber <quentingruber@gmail.com>
2022-03-25 23:49:10 +0100

    clear the timer started at StartLogoutRequest when clientLogout

--- src/servers/ZoneServer2015/zonepackethandlers.ts
+++ src/servers/ZoneServer2015/zonepackethandlers.ts
@@ -571,6 +571,7 @@ export class zonePacketHandlers {
       packet: any
     ) {
       debug("ClientLogout");
+      clearTimeout(client.hudTimer) // clear the timer started at StartLogoutRequest
       server.deleteClient(client);
     };
     this.GameTimeSync = function (
