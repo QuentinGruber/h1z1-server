4f7433e8db63864a52c4cd30c818077beed11354
Quentin Gruber <quentingruber@gmail.com>
2022-04-21 13:33:46 +0200

    Better handling of parse errors for H1z1Protocol and GatewayProtocol

--- src/types/protocols.ts
+++ src/types/protocols.ts
@@ -7,3 +7,17 @@ export interface LoginProtocolReadingFormat {
     result: any,
     type?: number,
 }
+
+export interface H1z1ProtocolReadingFormat {
+    name: string,
+    data: any,
+}
+
+export interface GatewayProtocolReadingFormat {
+    type: number,
+    flags: number,
+    fromClient?: boolean,
+    tunnelData?: Buffer
+    name: string,
+    result?: any,
+}
