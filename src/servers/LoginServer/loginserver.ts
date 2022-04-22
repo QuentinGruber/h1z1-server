8004eea9d32827470146c874d0c33c123fb30902
Quentin Gruber <quentingruber@gmail.com>
2022-04-22 18:53:16 +0200

    fix wrong name for env var SOLO_PLAY_IP

--- src/servers/LoginServer/loginserver.ts
+++ src/servers/LoginServer/loginserver.ts
@@ -106,7 +106,7 @@ export class LoginServer extends EventEmitter {
   _internalReqCount: number = 0;
   _pendingInternalReq: { [requestId: number]: any } = {};
   _pendingInternalReqTimeouts: { [requestId: number]: NodeJS.Timeout } = {};
-  private _soloPlayIp: string = process.env.SoloPlayIp || "127.0.0.1";
+  private _soloPlayIp: string = process.env.SOLO_PLAY_IP || "192.168.92.128";
 
   constructor(serverPort: number, mongoAddress = "") {
     super();
