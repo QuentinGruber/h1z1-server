5c815d9acdb838ca6c88a495ce44ba64a693913a
Quentin Gruber <quentingruber@gmail.com>
2022-04-24 17:28:44 +0200

    rm loginServer-2016 binary since it's the same as loginServer

--- bin/build.bat
+++ bin/build.bat
@@ -1,6 +1,5 @@
-START npx pkg --compress GZip -t node16-win-x64 ./2015/loginServer.js --output ./loginServer.exe
+START npx pkg --compress GZip -t node16-win-x64 ./shared/loginServer.js --output ./loginServer.exe
 START npx pkg --compress GZip -t node16-win-x64 ./2015/zoneServer.js --output ./zoneServer.exe
 START npx pkg --compress GZip -t node16-win-x64 ./2015/h1emuServer.js --output ./h1emuServer.exe
-START npx pkg --compress GZip -t node16-win-x64 ./2016/loginServer.js --output ./loginServer-2016.exe
 START npx pkg --compress GZip -t node16-win-x64 ./2016/zoneServer.js --output ./zoneServer-2016.exe
 START npx pkg --compress GZip -t node16-win-x64 ./2016/h1emuServer.js --output ./h1emuServer-2016.exe
