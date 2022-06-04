94c11b51b534dc574c2af4ee1e001a0db81cbf20
Quentin Gruber <quentingruber@gmail.com>
2022-06-04 16:27:34 +0200

    fix signatures

--- src/servers/ZoneServer2016/workers/Planting/Model/DataModels.ts
+++ src/servers/ZoneServer2016/workers/Planting/Model/DataModels.ts
@@ -1,3 +1,16 @@
+// ======================================================================
+//
+//   GNU GENERAL PUBLIC LICENSE
+//   Version 3, 29 June 2007
+//   copyright (C) 2020 - 2021 Quentin Gruber
+//   copyright (C) 2021 - 2022 H1emu community
+//
+//   https://github.com/QuentinGruber/h1z1-server
+//   https://www.npmjs.com/package/h1z1-server
+//
+//   Based on https://github.com/psemu/soe-network
+// ======================================================================
+
 //The furrows contains 2 furrow and also 4 holes in that;
 import { Euler, Vector4 } from "./TypeModels";
 import { MoveToByParent } from "../Utils";
