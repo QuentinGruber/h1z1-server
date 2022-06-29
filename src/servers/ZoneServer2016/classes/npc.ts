1c49d882ec9d0ee80e5ddcc5ab069557b5057789
Avcio <79534197+avcio9@users.noreply.github.com>
2022-06-30 00:37:48 +0200

    set npc flags

--- src/servers/ZoneServer2016/classes/npc.ts
+++ src/servers/ZoneServer2016/classes/npc.ts
@@ -13,19 +13,32 @@
 
 import { BaseFullCharacter } from "./basefullcharacter";
 
+function getFlags(field: any) {
+    let flagValue = 0;
+    for (let j = 0; j < field.length; j++) {
+        const flag = field[j];
+        if (flag) {
+            flagValue = flagValue | (1 << j);
+        }
+    }
+    return flagValue
+}
+
 export class Npc extends BaseFullCharacter {
-  npcRenderDistance = 80;
-  spawnerId: number;
-  flags = { a: 0, b: 127, c: 0 };
-  constructor(
-    characterId: string,
-    transientId: number,
-    actorModelId: number,
-    position: Float32Array,
-    rotation: Float32Array,
-    spawnerId: number = 0
-  ) {
-    super(characterId, transientId, actorModelId, position, rotation);
-    this.spawnerId = spawnerId;
-  }
+    npcRenderDistance = 80;
+    spawnerId: number;
+    constructor(
+        characterId: string,
+        transientId: number,
+        actorModelId: number,
+        position: Float32Array,
+        rotation: Float32Array,
+        spawnerId: number = 0
+    ) {
+        super(characterId, transientId, actorModelId, position, rotation);
+        this.spawnerId = spawnerId;
+        this.flags.a = getFlags([false, false, false, false, false, false, false, false]);
+        this.flags.b = getFlags([false, false, false, false, true, false, false, false]);
+        this.flags.c = getFlags([false, false, false, false, false, false, false, false]);
+    }
 }
