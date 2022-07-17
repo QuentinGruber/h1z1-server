8e560426aeffbde85f4abc2e0b1056067893e2a5
Avcio <79534197+avcio9@users.noreply.github.com>
2022-07-17 12:45:29 +0200

    add quat2matrix

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -93,6 +93,26 @@ export function eul2quat(angle: Float32Array): Float32Array {
   return new Float32Array([qx, qy, -qz, qw]);
 }
 
+export function quat2matrix(angle: Float32Array): any {
+  // a little modified for my needs, may not work for other things than construction
+    var x = angle[0];
+    var y = angle[1];
+    var z = angle[2];
+    var w = 0;
+
+    var n = w * w + x * x + y * y + z * z;
+    var s = n === 0 ? 0 : 2 / n;
+    var wx = s * w * x, wy = s * w * y, wz = s * w * z;
+    var xx = s * x * x, xy = s * x * y, xz = s * x * z;
+    var yy = s * y * y, yz = s * y * z, zz = s * z * z;
+
+
+    return [
+        1 - (yy + zz), xy - wz, xz + wy,
+        xy + wz, 1 - (xx + zz), yz - wx,
+        xz - wy, yz + wx, 1 - (xx + yy)];
+}
+
 export function eul2quatLegacy(angle: number[]) {
   // Assuming the angles are in radians.
   const heading = angle[0],
