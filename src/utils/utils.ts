c8f78d1d9ec1bfbb359e453c3d8697380e982643
Avcio <79534197+avcio9@users.noreply.github.com>
2022-07-28 13:08:08 +0200

    add isInside func to utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -204,6 +204,22 @@ const isBetween = (radius: number, value1: number, value2: number): boolean => {
   return value1 <= value2 + radius && value1 >= value2 - radius;
 };
 
+export const isInside = (point: [number, number], vs: any) => {
+
+    var x = point[0], y = point[1];
+
+    var inside = false;
+    for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+        var xi = vs[i][0], yi = vs[i][1];
+        var xj = vs[j][0], yj = vs[j][1];
+
+        var intersect = ((yi > y) != (yj > y))
+            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) inside = !inside;
+    }
+    return inside;
+};
+
 export const isPosInRadius = (
   radius: number,
   player_position: Float32Array,
