21397286179be7e22c4eb11614af858d61981f73
Cory M <csm4502@gmail.com>
2022-09-04 16:35:15 -0400

    cleanup isInside, stop using var keyword

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -224,15 +224,14 @@ const isBetween = (radius: number, value1: number, value2: number): boolean => {
 
 export const isInside = (point: [number, number], vs: any) => {
 
-    var x = point[0], y = point[1];
+    const x = point[0], y = point[1];
 
-    var inside = false;
-    for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
-        var xi = vs[i][0], yi = vs[i][1];
-        var xj = vs[j][0], yj = vs[j][1];
-
-        var intersect = ((yi > y) != (yj > y))
-            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+    let inside = false;
+    for (let i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+        const xi = vs[i][0], yi = vs[i][1],
+        xj = vs[j][0], yj = vs[j][1],
+        intersect = ((yi > y) != (yj > y))
+          && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
         if (intersect) inside = !inside;
     }
     return inside;
