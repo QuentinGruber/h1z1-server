f8e90112d772a7b35f143daf12ca0eebb2a2c275
Qgruber <47059878+QuentinGruber@users.noreply.github.com>
2022-10-03 14:20:20 +0000

    Close Gen Login/gateway/zone packets type for sendData typing #1084

--- scripts/genPacketsInterfaces.ts
+++ scripts/genPacketsInterfaces.ts
@@ -52,6 +52,7 @@ function getSchemaBody(schema: any) {
 
 function writeInterface(packets: any, name: string) {
   let packetsInterfaces = "";
+  const packetsIntercacesNames:string[] = []
   Object.values(packets).forEach((packet: any) => {
     const { schema } = packet;
     let name;
@@ -62,11 +63,13 @@ function writeInterface(packets: any, name: string) {
     }
     let packetInterfaceString = "export interface " + name + " {\n";
     if (schema) {
+      packetsIntercacesNames.push(name);
       packetInterfaceString += getSchemaBody(schema);
       packetInterfaceString += "}\n";
       packetsInterfaces += packetInterfaceString;
     }
   });
+  packetsInterfaces += `export type ${name} = ${packetsIntercacesNames.join(" | ")};`
   fs.writeFileSync(`./src/types/${name}.ts`, packetsInterfaces);
 }
 
