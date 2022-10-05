0a9b91cafdd310a1f503673cf97dcf1f9ebd7394
Cory M <csm4502@gmail.com>
2022-10-04 22:05:06 -0400

    fix merge error

--- src/types/packets.ts
+++ src/types/packets.ts
@@ -2491,44 +2491,6 @@ export type h1z1PacketsType =
   | "ClientPath.Reply"
   | "Ui.ConfirmHit";
 
-export type weaponPacketsType =
-  | "Weapon.FireStateUpdate"
-  | "Weapon.FireStateTargetedUpdate"
-  | "Weapon.Fire"
-  | "Weapon.FireWithDefinitionMapping"
-  | "Weapon.FireNoProjectile"
-  | "Weapon.ProjectileHitReport"
-  | "Weapon.ReloadRequest"
-  | "Weapon.Reload"
-  | "Weapon.ReloadInterrupt"
-  | "Weapon.ReloadRejected"
-  | "Weapon.SwitchFireModeRequest"
-  | "Weapon.LockOnGuidUpdate"
-  | "Weapon.LockOnLocationUpdate"
-  | "Weapon.StatUpdate"
-  | "Weapon.AddFireGroup"
-  | "Weapon.RemoveFireGroup"
-  | "Weapon.ReplaceFireGroup"
-  | "Weapon.GuidedUpdate"
-  | "Weapon.Reset";
-
-export type remoteWeaponPacketsType = 
-  | "RemoteWeapon.Reset"
-  | "RemoteWeapon.AddWeapon"
-  | "RemoteWeapon.RemoveWeapon";
-
-export type remoteWeaponUpdatePacketsType =
-  | "Update.ProjectileLaunch"
-  | "Update.Reload"
-  | "Update.AddFireGroup"
-  | "Update.FireState"
-  | "Update.SwitchFireMode"
-  | "Update.ReloadLoopEnd"
-  | "Update.ReloadInterrupt"
-  | "Update.AimBlocked"
-  | "Update.Empty"
-  | "Update.Chamber";
-
 export type loginPacketsType =
   | "LoginRequest"
   | "LoginReply"
