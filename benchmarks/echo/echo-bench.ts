f2f9e31e30d83e3d671968ffa9e945e194334af5
Quentin Gruber <quentingruber@gmail.com>
2022-11-14 18:44:44 +0100

    echo bench setup + add option to disable antiddos

--- benchmarks/echo/echo-bench.ts
+++ benchmarks/echo/echo-bench.ts
@@ -1,11 +1,28 @@
-import { EchoServer } from "./echo-server"
-import { EchoClient } from "./echo-client"
+import { EchoServer } from "./echo-server";
+import { BenchParameters, EchoClient } from "./echo-client";
 
 const cryptoKey = Buffer.from("F70IaxuU8C/w7FPXY1ibXw==", "base64");
-const echoServer = new EchoServer(1119,cryptoKey)
+const echoServer = new EchoServer(1119, cryptoKey);
 
-echoServer.start()
+// uncomment this to disable multiPackets
+// echoServer._waitQueueTimeMs = 0;
 
-const echoClient = new EchoClient("127.0.0.1",1119)
+echoServer.start();
 
-echoClient.sendSessionRequest()
+const benchParameters: BenchParameters = {
+  packetsToExchange: 1000,
+  packetsAtATime: 50,
+  stopTimerOnAllAcked: true,
+  bytesPerPacket: 200,
+};
+const echoClient = new EchoClient(1119, benchParameters);
+
+echoClient.sendSessionRequest();
+
+async function echoAwait() {
+  const finalTime = await echoClient.getFinalTime();
+  console.log(`Took ${finalTime}ms`);
+  process.exit(0);
+}
+
+echoAwait();
--- benchmarks/echo/echo-client.ts
+++ benchmarks/echo/echo-client.ts
@@ -1,81 +1,182 @@
-
 import { Worker } from "worker_threads";
-import { Soeprotocol }  from "h1emu-core"
+import { Soeprotocol } from "h1emu-core";
+import { Scheduler } from "../../out/utils/utils";
 interface ServerTarger {
-	address:string;
-	port: number;
+  address: string;
+  port: number;
+}
+
+export interface BenchParameters {
+  packetsToExchange: number;
+  packetsAtATime: number;
+  stopTimerOnAllAcked: boolean;
+  bytesPerPacket: number;
 }
 export class EchoClient {
-    private _connection: Worker;
-		private _serverTarget: ServerTarger; 
-		private _protocol: Soeprotocol
-		private _sequenceNumber: number = 0;
-		constructor(serverAddress:string,serverPort:number){
-		this._serverTarget = {address:serverAddress,port:serverPort}
-		this._protocol = new Soeprotocol(false,0);
+  private _connection: Worker;
+  private _serverTarget: ServerTarger;
+  private _protocol: Soeprotocol;
+  private _sequenceNumber: number = 0;
+  private _benchParameters: BenchParameters;
+  private _receiveEchoedPackets: number = 0;
+  private _timerStartTime: number = 0;
+  private _finalTime: any;
+  private _isRunning: boolean = true;
+  private _receivePacketsFromBundle: number = 0;
+  private _lastAck: number = 0;
+  private _dummyData: number[] = [];
+  constructor(serverPort: number, benchParameters: BenchParameters) {
+    this._benchParameters = benchParameters;
+    this._dummyData = this.genDummyData();
+    this._serverTarget = { address: "127.0.0.1", port: serverPort };
+    this._protocol = new Soeprotocol(false, 0);
     this._connection = new Worker(
       `${__dirname}/../../out/servers/shared/workers/udpServerWorker.js`,
       {
-        workerData: { serverPort: 0},
+        workerData: { serverPort: 0, disableAntiDdos: true },
       }
     );
-		
+
     this._connection.on("message", (message) => {
       const data = message.data;
-			this.receiveData(data);
-			})
-	}
-	sendSessionRequest(){
-		const sessionRequestPacket = this._protocol.pack_session_request_packet(1245,0,512,"Echo");
-		this._sendPhysicalPacket(sessionRequestPacket);
-		console.log("send session request")
-	}
-	
-	private _sendPhysicalPacket(packet: Uint8Array): void {
-	  this._connection.postMessage(
-	    {
-	      type: "sendPacket",
-	      data: {
-	        packetData: packet,
-	        length: packet.length,
-	        port: this._serverTarget.port,
-	        address: this._serverTarget.address,
-	      },
-	    }
-	  );
-	}
-	
-	sendData(): void{
-		const data = new Uint8Array([1,2,3,4,5,6,7,8,9,10,100185,485188,5428,418])
-	  const dataPacket = this._protocol.pack_data_packet(data,this._sequenceNumber)
-		this._sequenceNumber++;
-		this._sendPhysicalPacket(dataPacket);
-	}	
-	
-	handlePacket(packet:any): void{
-  	switch(packet.name){
-			case "SessionReply":
-				console.log("sessionreply received")
-				this.sendData();
-				break;
-			case "MultiPacket":
+      this.receiveData(data);
+    });
+  }
+  genDummyData(): number[] {
+    const dummy:number[] = [];
+    for (let i = 0; i < this._benchParameters.bytesPerPacket; i++) {
+      dummy.push(0xff);
+    }
+    return dummy;
+  }
+  sendSessionRequest() {
+    const sessionRequestPacket = this._protocol.pack_session_request_packet(
+      1245,
+      0,
+      512,
+      "Echo"
+    );
+    this._sendPhysicalPacket(sessionRequestPacket);
+  }
+
+  private _sendPhysicalPacket(packet: Uint8Array): void {
+    this._connection.postMessage({
+      type: "sendPacket",
+      data: {
+        packetData: packet,
+        length: packet.length,
+        port: this._serverTarget.port,
+        address: this._serverTarget.address,
+      },
+    });
+  }
+
+  sendMultipleDataPackets() {
+    const { packetsAtATime } = this._benchParameters;
+    const packetsLeftsToSend =
+      this._benchParameters.packetsToExchange - this._receiveEchoedPackets;
+    const packetsToSend =
+      packetsLeftsToSend > packetsAtATime ? packetsAtATime : packetsLeftsToSend;
+    for (let i = 0; i < packetsToSend; i++) {
+      this.sendData();
+    }
+  }
+
+  getData(): Uint8Array {
+    return new Uint8Array(this._dummyData);
+  }
+
+  getPackedData() {
+    const data = this.getData();
+    const dataPacket = this._protocol.pack_data_packet(
+      data,
+      this._sequenceNumber
+    );
+    this._sequenceNumber++;
+    return dataPacket;
+  }
+  sendData(): void {
+    this._sendPhysicalPacket(this.getPackedData());
+  }
+
+  startTimer() {
+    this._timerStartTime = Date.now();
+  } 
+
+  stopTimer() {
+    this._finalTime = Date.now() - this._timerStartTime;
+  }
+
+  async getFinalTime() {
+    while (!this._finalTime) {
+      await Scheduler.yield();
+    }
+    return this._finalTime;
+  }
+
+  hasFinish(): boolean {
+    const allPacketsEchoed =
+      this._receiveEchoedPackets === this._benchParameters.packetsToExchange;
+
+    const allPacketsAcked =
+      this._lastAck === this._benchParameters.packetsToExchange - 1;
+
+    return (
+      allPacketsEchoed &&
+      (!this._benchParameters.stopTimerOnAllAcked || allPacketsAcked)
+    );
+  }
+
+  finish() {
+    this._isRunning = false;
+    this.stopTimer();
+  }
+
+  handlePacket(packet: any): void {
+    if (!this._isRunning) {
+      return;
+    }
+    switch (packet.name) {
+      case "SessionReply":
+        this.sendMultipleDataPackets();
+        this.startTimer();
+        break;
+      case "MultiPacket":
         for (let i = 0; i < packet.sub_packets.length; i++) {
           const subPacket = packet.sub_packets[i];
           this.handlePacket(subPacket);
-          }
-				break;
-			case "Data":
-				this.sendData();
-				break;
-			case "Ack":
-				break;
-		}
-	}
-	
-	receiveData(data:Uint8Array): void{
-		const raw_parsed_data: string = this._protocol.parse(data);
-		console.log("client received ",data)
-		const parsed_data = JSON.parse(raw_parsed_data);
-		this.handlePacket(parsed_data);
-	}
+        }
+        break;
+      case "Data":
+        this._receiveEchoedPackets++;
+        this._receivePacketsFromBundle++;
+        if (this.hasFinish()) {
+          this.finish();
+        } else if (
+          this._receivePacketsFromBundle ===
+          this._benchParameters.packetsAtATime
+        ) {
+          this._receivePacketsFromBundle = 0;
+          this.sendMultipleDataPackets();
+        }
+        this.sendAck(packet.sequence);
+        break;
+      case "Ack":
+        this._lastAck = packet.sequence;
+        if (this.hasFinish()) {
+          this.finish();
+        }
+        break;
+    }
+  }
+  sendAck(sequence: number) {
+    const ackPacket = this._protocol.pack_ack_packet(sequence);
+    this._sendPhysicalPacket(ackPacket);
+  }
+
+  receiveData(data: Uint8Array): void {
+    const raw_parsed_data: string = this._protocol.parse(data);
+    const parsed_data = JSON.parse(raw_parsed_data);
+    this.handlePacket(parsed_data);
+  }
 }
