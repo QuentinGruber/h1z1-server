b3ffbea319705a64d06420cad71272f0ad2b5ce1
avcio9 <avcio2q@gmail.com>
2023-01-18 21:54:55 +0100

    added generic pickup sound for plants

--- src/servers/ZoneServer2016/entities/plant.ts
+++ src/servers/ZoneServer2016/entities/plant.ts
@@ -135,7 +135,11 @@ export class Plant extends ItemObject {
         );
         break;
     }
-
+    server.sendCompositeEffectToAllWithSpawnedEntity(
+      server._plants,
+        this,
+        5151     
+    );
     server.deleteEntity(this.characterId, server._plants);
   }
 
