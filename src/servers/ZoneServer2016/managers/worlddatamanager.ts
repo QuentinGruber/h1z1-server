d870b1aac4344a3065a72eb8c1845ce27eb11587
Quentin Gruber <quentingruber@gmail.com>
2023-01-23 21:28:42 +0100

    prettier

--- src/servers/ZoneServer2016/managers/worlddatamanager.ts
+++ src/servers/ZoneServer2016/managers/worlddatamanager.ts
@@ -819,9 +819,16 @@ export class WorldDataManager {
         return;
       }
     } else {
-      const hasTempData = Boolean((await server._db?.collection("construction-temp").find({serverId:server._worldId}).toArray())?.length)
-      if(hasTempData){
-        throw "Database still have temp data for this worldId"
+      const hasTempData = Boolean(
+        (
+          await server._db
+            ?.collection("construction-temp")
+            .find({ serverId: server._worldId })
+            .toArray()
+        )?.length
+      );
+      if (hasTempData) {
+        throw "Database still have temp data for this worldId";
       }
       constructionParents = <any>(
         await server._db
@@ -1104,9 +1111,16 @@ export class WorldDataManager {
         return;
       }
     } else {
-      const hasTempData = Boolean((await server._db?.collection("crop-temp").find({serverId:server._worldId}).toArray())?.length)
-      if(hasTempData){
-        throw "Database still have temp data for this worldId"
+      const hasTempData = Boolean(
+        (
+          await server._db
+            ?.collection("crop-temp")
+            .find({ serverId: server._worldId })
+            .toArray()
+        )?.length
+      );
+      if (hasTempData) {
+        throw "Database still have temp data for this worldId";
       }
       crops = <any>(
         await server._db
@@ -1153,9 +1167,16 @@ export class WorldDataManager {
         return;
       }
     } else {
-      const hasTempData = Boolean((await server._db?.collection("worldconstruction-temp").find({serverId:server._worldId}).toArray())?.length)
-      if(hasTempData){
-        throw "Database still have temp data for this worldId"
+      const hasTempData = Boolean(
+        (
+          await server._db
+            ?.collection("worldconstruction-temp")
+            .find({ serverId: server._worldId })
+            .toArray()
+        )?.length
+      );
+      if (hasTempData) {
+        throw "Database still have temp data for this worldId";
       }
       freeplace = <any>(
         await server._db
