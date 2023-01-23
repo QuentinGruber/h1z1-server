a9641504ffc0b27d7b26aaf949d9b93b2059abef
avcio9 <avcio2q@gmail.com>
2023-01-23 20:09:29 +0100

    adjusted render distance values

--- src/servers/ZoneServer2016/entities/constructionchildentity.ts
+++ src/servers/ZoneServer2016/entities/constructionchildentity.ts
@@ -6,36 +6,26 @@
 //   Based on https://github.com/psemu/soe-network
 // ======================================================================
 
-function getRenderDistance(actorModelId: number) {
+function getRenderDistance(itemDefinitionId: number) {
   let range: number = 0;
-  switch (actorModelId) {
-    case 9115: // tamper
-      range = 1000;
+  switch (itemDefinitionId) {
+    case Items.SHACK: // metal shack
+    case Items.SHACK_SMALL: // small shack  
+    case Items.SHACK_BASIC: // wood shack
+    case Items.FOUNDATION: // foundation,
+    case Items.FOUNDATION_EXPANSION: // expansion
+    case Items.GROUND_TAMPER: // tamper
+      range = 700;
       break;
-    case 9492: // expansion
-    case 9181: // shack door
-    case 9180: // metal shack
-    case 9192: // small shack
-    case 55: // dew collector
-    case 9223: // wood shack
-    case 63: // wood shack door
-      range = 500;
+    case Items.FURNACE:
+    case Items.WORKBENCH:
+    case Items.WORKBENCH_WEAPON:
+    case Items.BEE_BOX:
+    case Items.DEW_COLLECTOR:
+      range = 200;
       break;
-    case 9487: // ramp
-      range = 450;
-      break;
-    case 9488: // foundation stairs
-    case 49: // metal gate
-    case 50: // metal wall
-    case 9407: // upper metal wall
-    case 51: // shelter
-    case 52: // large shelter
-    case 9408: // upper level shelter
-    case 9411: // upper level large shelter
-    case 53: // structure stairs
-    case 9493: // tower
-    case 9130: // foundation, lod distance is 2250, tho i dont think we need it to be that high
-      range = 750;
+    case Items.STORAGE_BOX:
+      range = 20;
       break;
     default:
       range = 750;
@@ -145,7 +135,7 @@ export class ConstructionChildEntity extends BaseLightweightCharacter {
     this.profileId = 999; /// mark as construction
     this.damageRange = getDamageRange(this.itemDefinitionId);
     this.isSecured = this.itemDefinitionId == Items.METAL_WALL ? true : false;
-    this.npcRenderDistance = getRenderDistance(actorModelId);
+    this.npcRenderDistance = getRenderDistance(this.itemDefinitionId);
 
     registerConstructionSlots(this, this.wallSlots, wallSlotDefinitions);
     Object.seal(this.wallSlots);
