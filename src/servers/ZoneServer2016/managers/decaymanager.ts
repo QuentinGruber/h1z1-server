abd3823bfab2ab80c18da09d1fc21645a1ce3cd3
avcio9 <avcio2q@gmail.com>
2023-01-31 14:13:53 +0100

    prettier

--- src/servers/ZoneServer2016/managers/decaymanager.ts
+++ src/servers/ZoneServer2016/managers/decaymanager.ts
@@ -18,43 +18,52 @@ import { ConstructionChildEntity } from "../entities/constructionchildentity";
 import { Scheduler } from "../../../utils/utils";
 
 export class DecayManager {
-    loopTime = 1200000; // 20 min
-    currentTicksCount = 0; // used to run structure damaging once every x loops
-    requiredTicksToDamage = 36 // damage structures once every 12 hours 
+  loopTime = 1200000; // 20 min
+  currentTicksCount = 0; // used to run structure damaging once every x loops
+  requiredTicksToDamage = 36; // damage structures once every 12 hours
 
-    public async run(server: ZoneServer2016) {
-        this.contructionExpirationCheck(server)
-        await Scheduler.wait(this.loopTime);
-        this.run(server);
-    }
+  public async run(server: ZoneServer2016) {
+    this.contructionExpirationCheck(server);
+    await Scheduler.wait(this.loopTime);
+    this.run(server);
+  }
 
-    private contructionExpirationCheck(server: ZoneServer2016) {
-        for (const a in server._constructionFoundations) {
-            const foundation = server._constructionFoundations[a]
-            if (foundation.itemDefinitionId == Items.FOUNDATION_EXPANSION) continue
-            if (Object.keys(foundation.occupiedWallSlots).length == 0 &&
-                Object.keys(foundation.occupiedShelterSlots).length == 0 &&
-                Object.keys(foundation.occupiedUpperWallSlots).length == 0) {
-                if (foundation.objectLessTicks >= foundation.objectLessMaxTicks) {
-                    for (const a in foundation.occupiedExpansionSlots) {
-                        const expansion = foundation.occupiedExpansionSlots[a]
-                        for (const a in expansion.occupiedRampSlots) {
-                            expansion.occupiedRampSlots[a].destroy(server)
-                        }
-                        expansion.destroy(server)
-                    }
-                    for (const a in foundation.occupiedRampSlots) {
-                        foundation.occupiedRampSlots[a].destroy(server)
-                    }
-                    Object.values(foundation.freeplaceEntities).forEach((entity: LootableConstructionEntity | ConstructionDoor | ConstructionChildEntity) => {
-                        entity.destroy(server)
-                    })
-                    foundation.destroy(server)
-                }
-                foundation.objectLessTicks++
-            } else {
-                foundation.objectLessTicks = 0
+  private contructionExpirationCheck(server: ZoneServer2016) {
+    for (const a in server._constructionFoundations) {
+      const foundation = server._constructionFoundations[a];
+      if (foundation.itemDefinitionId == Items.FOUNDATION_EXPANSION) continue;
+      if (
+        Object.keys(foundation.occupiedWallSlots).length == 0 &&
+        Object.keys(foundation.occupiedShelterSlots).length == 0 &&
+        Object.keys(foundation.occupiedUpperWallSlots).length == 0
+      ) {
+        if (foundation.objectLessTicks >= foundation.objectLessMaxTicks) {
+          for (const a in foundation.occupiedExpansionSlots) {
+            const expansion = foundation.occupiedExpansionSlots[a];
+            for (const a in expansion.occupiedRampSlots) {
+              expansion.occupiedRampSlots[a].destroy(server);
             }
+            expansion.destroy(server);
+          }
+          for (const a in foundation.occupiedRampSlots) {
+            foundation.occupiedRampSlots[a].destroy(server);
+          }
+          Object.values(foundation.freeplaceEntities).forEach(
+            (
+              entity:
+                | LootableConstructionEntity
+                | ConstructionDoor
+                | ConstructionChildEntity
+            ) => {
+              entity.destroy(server);
+            }
+          );
+          foundation.destroy(server);
         }
+        foundation.objectLessTicks++;
+      } else {
+        foundation.objectLessTicks = 0;
+      }
     }
+  }
 }
