2dfdad21371ed4853cd05c3a99247f46011f90d6
Quentin Gruber <quentingruber@gmail.com>
2023-03-09 01:05:10 +0100

    allow to use mongodb with h1z1-server demo

--- scripts/demo/loginserver2016.js
+++ scripts/demo/loginserver2016.js
@@ -1,7 +1,7 @@
 const H1Z1servers = require("../../h1z1-server");
 var server = new H1Z1servers.LoginServer(
   1115, // <- server port
-  "" // <- MongoDB address ( if blank server start in solo mode )
+  process.env.MONGO_URL
 );
 server._protocol = new H1Z1servers.LoginProtocol("LoginUdp_11");
 server.start();
