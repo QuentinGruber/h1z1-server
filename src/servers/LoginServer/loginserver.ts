9a2b05b2b631743334108c2f2036b9efc80d051d
Quentin Gruber <quentingruber@gmail.com>
2023-04-15 15:09:42 +0200

    rename verified =^> unverfied

--- src/servers/LoginServer/loginserver.ts
+++ src/servers/LoginServer/loginserver.ts
@@ -899,8 +899,8 @@ export class LoginServer extends EventEmitter {
       banInfos.push({ banInfo: BAN_INFO.HWID });
     }
 
-    if (await this.isClientVerified(client)) {
-      banInfos.push({ banInfo: BAN_INFO.VERIFIED });
+    if (!(await this.isClientVerified(client))) {
+      banInfos.push({ banInfo: BAN_INFO.UNVERIFIED });
     }
 
     return banInfos;
--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -22,7 +22,7 @@ export enum BAN_INFO {
   GLOBAL_BAN = 2,
   VPN = 3,
   HWID = 4,
-  VERIFIED = 5,
+  UNVERIFIED = 5,
 }
 
 export enum NAME_VALIDATION_STATUS {
