3fa4d80e47eff9abb6ae7f5e7e2c4432a7b9cb60
Z1Meme <csm4502@gmail.com>
2023-05-07 01:42:59 -0400

    fixed plastic shard recipe

--- src/servers/ZoneServer2016/data/Recipes.ts
+++ src/servers/ZoneServer2016/data/Recipes.ts
@@ -1830,10 +1830,11 @@ export const recipes: { [recipeId: number]: Recipe } = {
   },
   [Items.SHARD_PLASTIC]: {
     filterId: FilterIds.COMPONENT,
+    bundleCount: 5,
     components: [
       {
         itemDefinitionId: Items.WATER_EMPTY,
-        requiredAmount: 3,
+        requiredAmount: 1,
       },
     ],
   },
