1caf620e112cbb521fc51ea5e046a05f50ecfe5f
Z1Meme <csm4502@gmail.com>
2023-05-21 20:38:41 -0400

    mess of code

--- src/servers/ZoneServer2016/managers/pluginmanager.ts
+++ src/servers/ZoneServer2016/managers/pluginmanager.ts
@@ -2,8 +2,9 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as ts from 'typescript';
 import { ZoneServer2016 } from "../zoneserver";
+import * as vm from 'vm';
 
-export default abstract class BasePlugin {
+export abstract class BasePlugin {
   public abstract name: string;
   public abstract description: string;
   public abstract version: string;
@@ -21,31 +22,102 @@ export class PluginManager {
     }
   }
 
-  private loadPlugins(): void {
-    this.checkPluginsFolder();
-    const pluginFiles = fs.readdirSync(this.pluginDir).filter(file => file.endsWith('.ts'));
-
-    for (const file of pluginFiles) {
-      const filePath = path.join(this.pluginDir, file),
+  private async loadPlugin(file: string) {
+    const filePath = path.join(this.pluginDir, file),
       source = fs.readFileSync(filePath, 'utf-8'),
-      output = ts.transpileModule(source, { compilerOptions: { module: ts.ModuleKind.CommonJS } }),
-      module = eval(output.outputText);
+      output = ts.transpileModule(source, { compilerOptions: 
+        { 
+          module: ts.ModuleKind.CommonJS,
+          //baseUrl: "./h1z1-server",
+          paths: {
+            "@h1z1-server/*": ["./*"],
+          }, 
+        } });
 
-      if (module?.default?.prototype instanceof BasePlugin) {
-        const plugin = new module.default();
+
+      
+
+        async function runCode(compiledCode: string): Promise<any> {
+          const sandbox = {
+            require: require,
+            module: module,
+            exports: exports,
+          };
+        
+          const script = new vm.Script(compiledCode);
+          const context = vm.createContext(sandbox);
+          script.runInContext(context);
+        
+          return sandbox.module.exports;
+        }
+        
+        async function loadAndRunPlugin(compiledCode: string): Promise<any> {
+          return runCode(compiledCode);
+        }
+        runCode(output.outputText)
+          .then((result) => {
+            console.log('Plugin executed successfully:', result.default);
+            const test = new result.default();
+            console.log(test)
+            this.plugins.push(test);
+          })
+          .catch((error) => {
+            console.error('Error executing plugin:', error);
+          });
+
+
+
+      
+      //console.log(output.outputText)
+
+      //output.outputText = output.outputText.replace("@h1z1-server", "../h1z1-server");
+
+      //console.log(output.outputText)
+      
+      /*
+      const module = import(`${output.outputText}`).then((m)=> {
+        const test = new m();
+        console.log("Loaded!")
+        console.log(test)
+      });
+      */
+     
+      /*
+      if (true/*module?.default?.prototype instanceof BasePlugin*//*) {
+        const plugin = new module();
         this.plugins.push(plugin);
         console.log(`Loaded plugin: ${plugin.name}`);
       } else {
         console.warn(`Failed to load plugin ${file}`);
       }
+      */
+  }
+
+  private async loadPlugins() {
+    this.checkPluginsFolder();
+    const pluginFiles = fs.readdirSync(this.pluginDir).filter(file => file.endsWith('.ts'));
+
+    for (const file of pluginFiles) {
+      try {
+        await this.loadPlugin(file)
+      }
+      catch(e: any) {
+        console.error(e);
+      }
     }
   }
 
-  public initializePlugins(server: ZoneServer2016): void {
-    this.loadPlugins();
+  public async initializePlugins(server: ZoneServer2016) {
+    await this.loadPlugins();
 
     for (const plugin of this.plugins) {
-      plugin.init(server);
+      
+      try {
+        plugin.init(server);
+      }
+      catch(e: any) {
+        console.error(e);
+      }
     }
 
     if(this.plugins.length == 0) {
