6e05a9cb16ecba8f92aa464d940a402e5eeea702
quentingruber <quentingruber@gmail.com>
2023-08-19 05:55:36 +0200

    fix prettier

--- src/servers/ZoneServer2016/handlers/commands/commands.ts
+++ src/servers/ZoneServer2016/handlers/commands/commands.ts
@@ -1354,11 +1354,13 @@ export const commands: Array<Command> = [
         client,
         `Players: ${Object.values(server._clients)
           .map((c) => {
-            return `${c.character.name}: ${c.loginSessionId} | ${
-              server.getSoeClient(c.soeClientId)?.getNetworkStats()[2]
-            } | ${server.getSoeClient(c.soeClientId)?.getNetworkStats()[0]} | ${
-              server.getSoeClient(c.soeClientId)?.getNetworkStats()[1]
-            }`;
+            return `${c.character.name}: ${c.loginSessionId} | ${server
+              .getSoeClient(c.soeClientId)
+              ?.getNetworkStats()[2]} | ${server
+              .getSoeClient(c.soeClientId)
+              ?.getNetworkStats()[0]} | ${server
+              .getSoeClient(c.soeClientId)
+              ?.getNetworkStats()[1]}`;
           })
           .join(",\n")}`
       );
