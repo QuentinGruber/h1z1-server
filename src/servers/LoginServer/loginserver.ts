b73536a5e037ad836fa57259bccd6f51e4226894
Z1Meme <csm4502@gmail.com>
2023-10-14 05:33:22 -0400

    added solomode check for hadesQuery

--- src/servers/LoginServer/loginserver.ts
+++ src/servers/LoginServer/loginserver.ts
@@ -522,8 +522,10 @@ export class LoginServer extends EventEmitter {
     this.clients.set(client.soeClientId, client);
     this.sendData(client, "LoginReply", loginReply);
 
-    if (client.gameVersion === GAME_VERSIONS.H1Z1_6dec_2016) {
-      console.log("SENDING HADES PACKET")
+    if (
+      client.gameVersion == GAME_VERSIONS.H1Z1_6dec_2016 &&
+      !this._soloMode
+      ) {
       this.sendData(client, "H1emu.HadesQuery", {
         authTicket: "-",
         gatewayServer: "-"
