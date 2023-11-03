e172c84c66f684cffc8c4030b55f7b45f6874401
Z1Meme <csm4502@gmail.com>
2023-11-03 12:06:41 -0400

    Allow connections to old servers

--- src/servers/LoginServer/loginserver.ts
+++ src/servers/LoginServer/loginserver.ts
@@ -1060,6 +1060,14 @@ export class LoginServer extends EventEmitter {
         case CONNECTION_REJECTION_FLAGS.OTHER:
           reason = "OTHER";
           break;
+        default:
+          this.sendData(client, "H1emu.PrintToConsole", {
+            message: "Server may be running an old version, please report this to the server owner!",
+            showConsole: true,
+            clearOutput: false
+          });
+          this.sendData(client, "CharacterLoginReply", charactersLoginInfo);
+          return;
       }
       this.sendData(client, "H1emu.PrintToConsole", {
         message: `CONNECTION REJECTED! Reason: ${reason}`,
