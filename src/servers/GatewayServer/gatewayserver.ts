3b79bead79c2050aa5a70c8f65a81d48d15f313a
quentingruber <quentingruber@gmail.com>
2024-02-14 11:52:15 +0100

    Adapt gateway server for h1emu-core v2

--- src/servers/GatewayServer/gatewayserver.ts
+++ src/servers/GatewayServer/gatewayserver.ts
@@ -13,7 +13,13 @@
 
 import { EventEmitter } from "node:events";
 import { SOEServer } from "../SoeServer/soeserver";
-import { GatewayProtocol } from "h1emu-core";
+import {
+  GatewayOpcode,
+  GatewayProtocol,
+  LoginReplyPacket,
+  LoginRequestPacket,
+  TunnelPacket
+} from "h1emu-core";
 import SOEClient from "../SoeServer/soeclient";
 import { crc_length_options } from "../../types/soeserver";
 import { SOEOutputChannels } from "servers/SoeServer/soeoutputstream";
@@ -48,41 +54,42 @@ export class GatewayServer extends EventEmitter {
           return;
         }
         try {
-          const packet = JSON.parse(this._protocol.parse(data));
-          if (packet) {
-            switch (packet.name) {
-              case "LoginRequest":
-                if (packet.character_id) {
-                  this._soeServer.setEncryption(client, true);
-                  const appData = this._protocol.pack_login_reply_packet(true);
-                  if (appData) {
-                    this._soeServer.sendAppData(client, appData);
-                  }
-                  this.emit(
-                    "login",
-                    client.soeClientId,
-                    packet.character_id,
-                    packet.ticket,
-                    packet.client_protocol
-                  );
+          const packetParsed = this._protocol.parse(data);
+          switch (packetParsed.get_opcode()) {
+            case GatewayOpcode.LoginRequest:
+              const loginRequestPacket =
+                packetParsed.get_login_request_packet() as LoginRequestPacket;
+              if (loginRequestPacket.character_id) {
+                this._soeServer.setEncryption(client, true);
+                const loginReplyPacket = new LoginReplyPacket(true);
+                const appData = loginReplyPacket.build();
+                if (appData) {
+                  this._soeServer.sendAppData(client, appData);
                 }
-                break;
-              case "Logout":
-                debug("Logout gateway");
-                this.emit("disconnect", client.sessionId);
-                break;
-              case "TunnelPacket":
                 this.emit(
-                  "tunneldata",
-                  client.sessionId,
-                  Buffer.from(packet.tunnel_data),
-                  packet.channel
+                  "login",
+                  client.soeClientId,
+                  loginRequestPacket.character_id,
+                  loginRequestPacket.get_ticket(),
+                  loginRequestPacket.get_client_protocol()
                 );
-                break;
-            }
-          } else {
-            console.log(`Unsupported gateway packet ${packet.name}`);
-            console.log(packet);
+              }
+              break;
+            case GatewayOpcode.Logout:
+              debug("Logout gateway");
+              this.emit("disconnect", client.sessionId);
+              break;
+            case GatewayOpcode.TunnelDataClient:
+              const tunnelDataPacket =
+                packetParsed.get_tunnel_packet() as TunnelPacket;
+
+              this.emit(
+                "tunneldata",
+                client.sessionId,
+                Buffer.from(tunnelDataPacket.get_tunnel_data()),
+                tunnelDataPacket.channel
+              );
+              break;
           }
         } catch (e) {
           console.error("Gateway: packet parsing failed");
@@ -136,10 +143,12 @@ export class GatewayServer extends EventEmitter {
     channel: SOEOutputChannels
   ) {
     debug("Sending tunnel data to client");
-    const data = this._protocol.pack_tunnel_data_packet_for_client(
-      tunnelData,
-      0
+    const tunnelPacket = new TunnelPacket(
+      GatewayOpcode.TunnelDataServer,
+      0,
+      tunnelData
     );
+    const data = tunnelPacket.build();
     if (data) {
       const client = this._soeServer.getSoeClient(soeClientId);
       if (client) {
