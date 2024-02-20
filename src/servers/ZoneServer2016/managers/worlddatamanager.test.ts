80b10ac0202097ec56542da86d2d851d77ca410b
quentingruber <quentingruber@gmail.com>
2024-02-20 18:54:20 +0100

    Add some vehicles load for world save unit tests

--- src/servers/ZoneServer2016/managers/worlddatamanager.test.ts
+++ src/servers/ZoneServer2016/managers/worlddatamanager.test.ts
@@ -4,16 +4,22 @@ import assert from "assert";
 import { ZoneServer2016 } from "../zoneserver";
 
 const isMongoTests = process.env.MONGO_TESTS === "true";
-test("WorldDataManager", { timeout: 10000 }, async (t) => {
-  const zone = new ZoneServer2016(1118);
+
+async function worldSaveUnitTests(t: any, mongoAddress: string) {
+  const worldId = 81;
+  const zone = new ZoneServer2016(
+    0,
+    Buffer.from("fake"),
+    mongoAddress,
+    worldId
+  );
   await zone.start();
   const wdmanager = new WorldDataManager();
-  const worldId = 81;
   await t.test("check world save version", async () => {
     assert.equal(wdmanager.worldSaveVersion, 2);
   });
   await t.test("init world", async () => {
-    await wdmanager.initialize(worldId, "");
+    await wdmanager.initialize(worldId, mongoAddress);
   });
   const world: WorldArg = {
     lastGuidItem: 1000n,
@@ -23,13 +29,12 @@ test("WorldDataManager", { timeout: 10000 }, async (t) => {
     worldConstructions: [],
     crops: []
   };
-  // TODO: need investigation
-  // await t.test("convert vehicles", async () => {
-  //   world.vehicles = WorldDataManager.convertVehiclesToSaveData(
-  //     Object.values(zone._vehicles),
-  //     worldId
-  //   );
-  // });
+  await t.test("convert vehicles", async () => {
+    world.vehicles = WorldDataManager.convertVehiclesToSaveData(
+      Object.values(zone._vehicles),
+      worldId
+    );
+  });
   await t.test("save world", async () => {
     await wdmanager.saveWorld({ ...world });
   });
@@ -43,7 +48,10 @@ test("WorldDataManager", { timeout: 10000 }, async (t) => {
   });
   await t.test("load vehicles", async () => {
     const loadedVehicles = await wdmanager.loadVehiclesData();
-    assert.deepEqual(loadedVehicles, world.vehicles);
+    assert.deepEqual(
+      JSON.stringify(loadedVehicles),
+      JSON.stringify(world.vehicles)
+    );
   });
   await t.test("load constructions", async () => {
     const loadedConstructions = await wdmanager.loadConstructionData();
@@ -58,6 +66,9 @@ test("WorldDataManager", { timeout: 10000 }, async (t) => {
     const loadedCrops = await wdmanager.loadCropData();
     assert.deepEqual(loadedCrops, world.crops);
   });
+}
+test("WorldDataManager", { timeout: 10000 }, async (t) => {
+  await worldSaveUnitTests(t, "");
 });
 after(() => {
   setImmediate(() => {
@@ -69,49 +80,7 @@ test(
   "WorldDataManager-mongo",
   { timeout: 10000, skip: !isMongoTests },
   async (t) => {
-    const wdmanager = new WorldDataManager();
-    const worldId = 81;
-    await t.test("check world save version", async () => {
-      assert.equal(wdmanager.worldSaveVersion, 2);
-    });
-    const world: WorldArg = {
-      lastGuidItem: 1000n,
-      vehicles: [],
-      characters: [],
-      constructions: [],
-      worldConstructions: [],
-      crops: []
-    };
-    await t.test("init world", async () => {
-      await wdmanager.initialize(worldId, "mongodb://localhost:27017");
-      await wdmanager.insertWorld(world.lastGuidItem);
-    });
-    await t.test("save world", async () => {
-      await wdmanager.saveWorld({ ...world });
-    });
-
-    await t.test("load world data", async () => {
-      const loadedWorldData = await wdmanager.getServerData(worldId);
-      assert.deepEqual(loadedWorldData?.serverId, worldId);
-      assert.deepEqual(loadedWorldData?.lastItemGuid, world.lastGuidItem);
-    });
-    await t.test("load vehicles", async () => {
-      const loadedVehicles = await wdmanager.loadVehiclesData();
-      assert.deepEqual(loadedVehicles, world.vehicles);
-    });
-    await t.test("load constructions", async () => {
-      const loadedConstructions = await wdmanager.loadConstructionData();
-      assert.deepEqual(loadedConstructions, world.constructions);
-    });
-    await t.test("load world constructions", async () => {
-      const loadedWorldConstructions =
-        await wdmanager.loadWorldFreeplaceConstruction();
-      assert.deepEqual(loadedWorldConstructions, world.worldConstructions);
-    });
-    await t.test("load crops", async () => {
-      const loadedCrops = await wdmanager.loadCropData();
-      assert.deepEqual(loadedCrops, world.crops);
-    });
+    await worldSaveUnitTests(t, "mongodb://localhost:27017");
   }
 );
 after(() => {
