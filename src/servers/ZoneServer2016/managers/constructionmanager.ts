0b9a22cb51c2bf2bcaf101e97dee4d9000cd7fce
Nikolassparrow <nikolasmonroe361@gmail.com>
2024-02-25 14:51:02 -0600

    adjust bumjick poi to december 2016 range, allow shacks to be placed -in front-

--- src/servers/ZoneServer2016/managers/constructionmanager.ts
+++ src/servers/ZoneServer2016/managers/constructionmanager.ts
@@ -357,6 +357,7 @@ export class ConstructionManager {
     if (this.overridePlacementItems.includes(itemDefinitionId)) return false;
     let useRange = true;
     let isInPoi = false;
+    let isShackInRange = false;
     Z1_POIs.forEach((point: any) => {
       if (point.bounds) {
         useRange = false;
@@ -366,13 +367,20 @@ export class ConstructionManager {
             return;
           }
         });
+        if (point.shackBounds && this.shackItems.includes(itemDefinitionId)) {
+          point.shackBounds.forEach((bound: any) => {
+            if (isInsideSquare([position[0], position[2]], bound)) {
+              isShackInRange = true;
+            }
+          });
+        }
       }
       if (useRange && isPosInRadius(point.range, position, point.position)) {
         isInPoi = true;
       }
     });
     // allow placement in poi if object is parented to a foundation
-    if (isInPoi && !isInsidePermissionedFoundation) {
+    if (isInPoi && !isInsidePermissionedFoundation && !isShackInRange) {
       return true;
     }
     return false;
