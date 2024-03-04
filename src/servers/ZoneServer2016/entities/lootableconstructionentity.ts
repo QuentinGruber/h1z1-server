97eb3d11e704df8c06e07dd8f769032fc1c16dec
Nikolassparrow <nikolasmonroe361@gmail.com>
2024-03-04 14:12:17 -0600

    oop

--- src/servers/ZoneServer2016/entities/lootableconstructionentity.ts
+++ src/servers/ZoneServer2016/entities/lootableconstructionentity.ts
@@ -333,7 +333,7 @@ export class LootableConstructionEntity extends BaseLootableEntity {
     if (dictionary == server._worldLootableConstruction || server._worldSimpleConstruction && !this.parentObjectCharacterId) {
       freePlaceDmgMultiplier = 2;
     }
-    // 26 shots for freeplaced objects, 13 for parented objects
+    // 26 308 shots for freeplaced objects, 13 for parented objects
     const damage = damageInfo.damage * (3 * freePlaceDmgMultiplier);
     this.damage(server, { ...damageInfo, damage });
   }
