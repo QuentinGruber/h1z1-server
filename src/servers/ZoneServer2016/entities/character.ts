678b307175181905505a27ff12cf3671b9bd6497
Nikolassparrow <nikolasmonroe361@gmail.com>
2024-03-10 15:32:37 -0500

    prettier

--- src/servers/ZoneServer2016/entities/character.ts
+++ src/servers/ZoneServer2016/entities/character.ts
@@ -1047,7 +1047,8 @@ export class Character2016 extends BaseFullCharacter {
       oldHealth = this._resources[ResourceIds.HEALTH];
     if (!client) return;
 
-    if (this.isGodMode() || !this.isAlive || this.isRespawning || damage <= 25) return;
+    if (this.isGodMode() || !this.isAlive || this.isRespawning || damage <= 25)
+      return;
     if (damageInfo.causeBleed) {
       if (randomIntFromInterval(0, 100) < damage / 100 && damage > 500) {
         this._resources[ResourceIds.BLEEDING] += 41;
