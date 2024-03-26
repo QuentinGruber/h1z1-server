8b88908e422a89578a001860e80a007323dbda20
Nikolassparrow <nikolasmonroe361@gmail.com>
2024-03-25 19:51:58 -0500

    fix for #2041

--- src/servers/ZoneServer2016/managers/constructionmanager.ts
+++ src/servers/ZoneServer2016/managers/constructionmanager.ts
@@ -65,6 +65,7 @@ import {
   ConstructionUnknown,
   PlayerUpdatePosition
 } from "types/zone2016packets";
+import { SmeltingEntity } from "../classes/smeltingentity";
 
 export class ConstructionManager {
   overridePlacementItems: Array<number> = [
@@ -2539,13 +2540,28 @@ export class ConstructionManager {
     entity: ConstructionEntity,
     weaponItem: LoadoutItem
   ) {
-    switch (entity.itemDefinitionId) {
-      case Items.CAMPFIRE:
-      case Items.DEW_COLLECTOR:
-      case Items.BEE_BOX:
-      case Items.ANIMAL_TRAP:
-        return;
+    const disallowedItems = [
+      Items.CAMPFIRE,
+      Items.BEE_BOX,
+      Items.DEW_COLLECTOR,
+      Items.ANIMAL_TRAP
+    ];
+
+    const permission = entity.getHasPermission(
+      server, 
+      client.character.characterId, 
+      ConstructionPermissionIds.DEMOLISH
+    );
+
+    // check if the entity is a lootableconstruction type, isn't contained in disallowedItems ^
+    // and if the server is pve, allow only players with demolish permissions to break items with
+    // the crowbar - TODO: add a check for meleeing world entities on pve - niko
+    if (!(entity instanceof LootableConstructionEntity) || 
+    disallowedItems.includes(entity.itemDefinitionId) ||
+    (server.isPvE && !permission)) {
+      return;
     }
+
     let worldFreeplaceMultiplier = 1;
     const dictionary = server.getEntityDictionary(entity.characterId);
 
