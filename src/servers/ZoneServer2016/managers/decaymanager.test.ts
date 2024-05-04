ffaf1ffe4995e829e67db676c208f64eb2fcd117
quentingruber <quentingruber@gmail.com>
2024-05-04 12:19:36 +0200

    add normal decay test

--- src/servers/ZoneServer2016/managers/decaymanager.test.ts
+++ src/servers/ZoneServer2016/managers/decaymanager.test.ts
@@ -20,7 +20,10 @@ import { Items } from "../models/enums";
 test("decaymanager", { timeout: 10000 }, async (t) => {
   const zone = new ZoneServer2016(0);
   await zone.start();
+  const originalDate = Date.now();
+  t.mock.timers.enable({ apis: ["Date"] });
   await t.test("test decay grief", () => {
+    zone._constructionFoundations = {};
     const characterId = generate_random_guid();
     const transientId = zone.getTransientId(characterId);
     const pos = new Float32Array([0, 0, 0, 0]);
@@ -39,12 +42,38 @@ test("decaymanager", { timeout: 10000 }, async (t) => {
     );
     zone._constructionFoundations[characterId] = foundation;
 
-    const originalDate = Date.now();
-    t.mock.timers.enable({ apis: ["Date"] });
     // TODO: load that from config
-    const timeGrief = 60_000 * 60 * 24 * 3;
+    const timeGrief = 60_000 * 60 * 24 * 30;
     t.mock.timers.tick(originalDate + timeGrief);
     zone.decayManager.run(zone);
+    assert.strictEqual(Object.keys(zone._constructionFoundations).length, 0);
+  });
+  await t.test("test decay", () => {
+    zone._constructionFoundations = {};
+    const characterId = generate_random_guid();
+    const transientId = zone.getTransientId(characterId);
+    const pos = new Float32Array([0, 0, 0, 0]);
+    const foundation = new ConstructionParentEntity(
+      characterId,
+      transientId,
+      1,
+      pos,
+      pos,
+      zone,
+      // FIXME: that's an hack
+      1,
+      "1",
+      "name",
+      "",
+      ""
+    );
+    zone._constructionFoundations[characterId] = foundation;
+
+    zone.decayManager.griefCheckSlotAmount = 0;
+    t.mock.timers.tick(originalDate);
+    for (let i = 0; i < 10_000; i++) {
+      zone.decayManager.run(zone);
+    }
     assert.strictEqual(Object.keys(zone._constructionFoundations).length, 0);
   });
 });
