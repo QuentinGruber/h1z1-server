e66fc8ad403994ea65ec6d6f3311a057bb63995b
Jason Moonen <60873596+jasonmoonen@users.noreply.github.com>
2024-09-23 21:07:43 +0200

    Bring back population levels instead of numbers

--- src/servers/LoginServer/loginserver.ts
+++ src/servers/LoginServer/loginserver.ts
@@ -229,12 +229,20 @@ export class LoginServer extends EventEmitter {
                 case "UpdateZonePopulation": {
                   const { population } = packet.data;
                   const serverId = this._zoneConnections[client.clientId];
+                  const serverData = await this._db
+                      .collection(DB_COLLECTIONS.SERVERS)
+                      .findOne({ serverId: serverId });
+                  console.log(serverData?.maxPopulationNumber);
                   this._db?.collection(DB_COLLECTIONS.SERVERS).findOneAndUpdate(
                     { serverId: serverId },
                     {
                       $set: {
                         populationNumber: population,
-                        populationLevel: population
+                        populationLevel: Number(
+                          ((population / serverData?.maxPopulationNumber ?? 100) * 3).toFixed(
+                            0
+                          )
+                        ),
                       }
                     }
                   );
