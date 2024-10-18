63a73742ac67d25a244327338777610cb8fa0a91
Kornel Kaczmarek <avcio2q@gmail.com>
2024-10-18 04:13:22 +0200

    .

--- src/servers/ZoneServer2016/managers/fairplaymanager.ts
+++ src/servers/ZoneServer2016/managers/fairplaymanager.ts
@@ -756,7 +756,7 @@ export class FairPlayManager {
       return;
     }
 
-    const hashes = this.defaultHashes.concat(this.requiredPacks),
+      const hashes = this.requiredPacks.concat(this.defaultHashes),
       validatedHashes: Array<FileHash> = [];
 
     // check if all default / required packs are found in game files
