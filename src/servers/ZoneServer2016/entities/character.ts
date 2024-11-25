cec2a2bf694df117074e764cb421c87585a7c002
quentingruber <quentingruber@gmail.com>
2024-11-25 14:00:12 +0100

    change some checks and add logs

--- src/servers/ZoneServer2016/entities/character.ts
+++ src/servers/ZoneServer2016/entities/character.ts
@@ -1826,7 +1826,10 @@ export class Character2016 extends BaseFullCharacter {
   }
 
   OnMeleeHit(server: ZoneServer2016, damageInfo: DamageInfo) {
-    if (server.isPvE || this.isRespawning) return;
+    console.log("melee hit");
+    if ((server.isPvE && !server._npcs[damageInfo.entity]) || this.isRespawning)
+      return;
+    console.log("good");
     let damage = damageInfo.damage / 2;
     let bleedingChance = 5;
     switch (damageInfo.meleeType) {
