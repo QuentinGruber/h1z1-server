27ed67915ee7d273cc4d5c2ec6c4d1ef56a93c03
quentingruber <quentingruber@gmail.com>
2024-11-28 13:59:57 +0100

    use replaceOne instead of updateOne

--- src/servers/ZoneServer2016/managers/worlddatamanager.ts
+++ src/servers/ZoneServer2016/managers/worlddatamanager.ts
@@ -1037,9 +1037,9 @@ export class WorldDataManager {
       for (let i = 0; i < constructions.length; i++) {
         const construction = constructions[i];
         updatePromises.push(
-          collection.updateOne(
+          collection.replaceOne(
             { characterId: construction.characterId, serverId: this._worldId },
-            { $set: construction },
+            construction,
             { upsert: true }
           )
         );
