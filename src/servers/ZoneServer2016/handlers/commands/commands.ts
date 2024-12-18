caebad474cb0df694f9a86f5e1562df439b99e68
Nikolassparrow <nikolasmonroe361@gmail.com>
2024-12-18 17:10:03 -0600

    add

--- src/servers/ZoneServer2016/handlers/commands/commands.ts
+++ src/servers/ZoneServer2016/handlers/commands/commands.ts
@@ -2250,6 +2250,10 @@ export const commands: Array<Command> = [
           client.character.equipLoadout(server, characterKitLoadout, true);
           break;
         case "parts":
+          client.character.equipItem(
+            server,
+            server.generateItem(Items.FANNY_PACK_DEV)
+          );
           client.character.equipLoadout(server, characterVehicleKit, true);
           break;
         case "skins":
