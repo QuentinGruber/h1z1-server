24522bcf5c824887d3fe6d764aba09f4acf6f412
Ultrax <ujtjax@hotmail.com>
2024-12-21 14:14:53 +0100

    nerfed ferts

--- src/servers/ZoneServer2016/data/lootspawns.ts
+++ src/servers/ZoneServer2016/data/lootspawns.ts
@@ -1243,11 +1243,11 @@ export const lootTables: { [lootSpawner: string]: LootSpawner } = {
 
   // #region FARM
   "ItemSpawnerFarm.adr": {
-    spawnChance: 90,
+    spawnChance: 40,
     items: [
       {
         item: Items.FERTILIZER,
-        weight: 25,
+        weight: 23,
         spawnCount: {
           min: 1,
           max: 1
@@ -3614,12 +3614,12 @@ export const containerLootSpawners: {
   },
   // used for crate props
   Crate_buffed: {
-    spawnChance: 60,
+    spawnChance: 30,
     maxItems: 1,
     items: [
       {
         item: Items.FERTILIZER,
-        weight: 25,
+        weight: 22,
         spawnCount: {
           min: 1,
           max: 1
