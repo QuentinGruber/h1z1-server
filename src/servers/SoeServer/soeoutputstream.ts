f1076d052d71b7b313d2821a12bb47061793dc38
Kornel Kaczmarek <avcio2q@gmail.com>
2024-12-31 13:05:50 +0100

    disale isFlooded for now

--- src/servers/SoeServer/soeoutputstream.ts
+++ src/servers/SoeServer/soeoutputstream.ts
@@ -57,10 +57,11 @@ export class SOEOutputStream extends EventEmitter {
     }
   }
 
+  // basically disable for now
   isFlooded(): boolean {
-    const difference =
-      this._last_available_reliable_sequence.get() - this.lastAck.get();
-    return difference >= this.maxSequenceAvailable;
+    /*const difference =
+      this._last_available_reliable_sequence.get() - this.lastAck.get();*/
+    return false;
   }
 
   isReliableAvailable(): boolean {
