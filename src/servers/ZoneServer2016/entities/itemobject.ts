6ba89095d304307dcd14005070ebb627d42a31fd
Nikolassparrow <nikolasmonroe361@gmail.com>
2024-12-31 19:32:51 -0600

    prettier

--- src/servers/ZoneServer2016/entities/itemobject.ts
+++ src/servers/ZoneServer2016/entities/itemobject.ts
@@ -52,7 +52,7 @@ export class ItemObject extends BaseLightweightCharacter {
   ) {
     server.pickupItem(client, this.characterId);
     // 5 minutes to prevent abuse (players afk dropping & picking up the same item)
-    if (this.creationTime + 300_000  >= Date.now()) return;
+    if (this.creationTime + 300_000 >= Date.now()) return;
     server.lootCrateWithChance(client, 5);
   }
 
