458ce01caf64b03fc08ba8f46af1b58791db45dc
Nikolassparrow <nikolasmonroe361@gmail.com>
2025-01-05 12:44:52 -0600

    add

--- src/servers/ZoneServer2016/zoneserver.ts
+++ src/servers/ZoneServer2016/zoneserver.ts
@@ -8163,11 +8163,15 @@ export class ZoneServer2016 extends EventEmitter {
           this.divideMovementModifier(client, MovementModifiers.CONVEYS);
         }
 
-        if (itemDefinition.DESCRIPTION_ID == 11155 && !character.hasBoots) {
+        if (itemDefinition.DESCRIPTION_ID == 11155 || 
+          itemDefinition.DESCRIPTION_ID == 13581 && 
+          !character.hasBoots
+        ) {
           character.hasBoots = true;
           this.multiplyMovementModifier(client, MovementModifiers.BOOTS);
         } else if (
-          itemDefinition.DESCRIPTION_ID != 11895 &&
+          itemDefinition.DESCRIPTION_ID != 11155 &&
+          itemDefinition.DESCRIPTION_ID != 13581 &&
           character.hasBoots
         ) {
           character.hasBoots = false;
