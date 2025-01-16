416948b77e8e25c531f739f12b4d630ab1384293
Nikolassparrow <nikolasmonroe361@gmail.com>
2025-01-15 18:06:38 -0600

    fix

--- src/servers/ZoneServer2016/zonepackethandlers.ts
+++ src/servers/ZoneServer2016/zonepackethandlers.ts
@@ -3407,7 +3407,7 @@ export class ZonePacketHandlers {
           return;
         }
 
-        const newItem = server.generateItem(accountItem.REWARD_ITEM_ID),
+        const newItem = server.generateItem(accountItem.REWARD_ITEM_ID, 1, true),
           containerItems = client.character.getContainerFromGuid(
             oitem.itemGuid
           )?.items;
