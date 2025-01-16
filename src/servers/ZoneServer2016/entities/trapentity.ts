2fd4a819d5851aa032fbcade91c2457d02aa8e68
Nikolassparrow <nikolasmonroe361@gmail.com>
2025-01-15 22:08:48 -0600

    add

--- src/servers/ZoneServer2016/entities/trapentity.ts
+++ src/servers/ZoneServer2016/entities/trapentity.ts
@@ -97,7 +97,7 @@ export class TrapEntity extends BaseSimpleNpc {
               client.character.damage(server, {
                 entity: this.characterId,
                 causeBleed: true,
-                damage: 501
+                damage: !client.character.hasBoots ? 501 : 401
               });
               server.sendDataToAllWithSpawnedEntity(
                 server._traps,
