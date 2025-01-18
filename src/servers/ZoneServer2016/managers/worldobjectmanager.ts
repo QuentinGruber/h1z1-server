a3269e2c5efe45b264e40755c579a0d54c442baa
Nikolassparrow <nikolasmonroe361@gmail.com>
2025-01-18 15:58:46 -0600

    add

--- src/servers/ZoneServer2016/managers/worldobjectmanager.ts
+++ src/servers/ZoneServer2016/managers/worldobjectmanager.ts
@@ -32,7 +32,7 @@ import {
   getCurrentServerTimeWrapper,
   isLootNerfedLoc
 } from "../../../utils/utils";
-import { EquipSlots, Items, Effects, ModelIds } from "../models/enums";
+import { EquipSlots, Items, Effects, ModelIds, DefaultSkinsConveys, DefaultSkinsBackpack, DefaultSkinsMotorHelmet } from "../models/enums";
 import { Vehicle2016 } from "../entities/vehicle";
 import { LootDefinition } from "types/zoneserver";
 import { ItemObject } from "../entities/itemobject";
@@ -54,6 +54,30 @@ import { Npc } from "../entities/npc";
 //import { EntityType } from "h1emu-ai";
 import { scheduler } from "node:timers/promises";
 const debug = require("debug")("ZoneServer");
+
+export function getRandomSkin(itemDefinitionId: number) {
+  const allowedItems = [
+    Items.CONVEYS_BLUE,
+    Items.BACKPACK_BLUE_ORANGE,
+    Items.HELMET_MOTORCYCLE
+  ];
+  if (!allowedItems.includes(itemDefinitionId)) return itemDefinitionId;
+  let itemDefId = 0;
+  let arr: any[] = [];
+  switch (itemDefinitionId) {
+    case Items.CONVEYS_BLUE:
+      arr = Object.keys(DefaultSkinsConveys);
+      break;
+    case Items.BACKPACK_BLUE_ORANGE:
+      arr = Object.keys(DefaultSkinsBackpack);
+      break;
+    case Items.HELMET_MOTORCYCLE:
+      arr = Object.keys(DefaultSkinsMotorHelmet);
+      break;
+  }
+  itemDefId = Number(arr[Math.floor((Math.random() * arr.length) / 2)]);
+  return itemDefId;
+}
 
 export function getRandomItem(items: Array<LootDefinition>) {
   const totalWeight = items.reduce((total, item) => total + item.weight, 0),
@@ -984,7 +1008,7 @@ export class WorldObjectManager {
               this.createLootEntity(
                 server,
                 server.generateItem(
-                  item.item,
+                  getRandomSkin(item.item),
                   randomIntFromInterval(
                     item.spawnCount.min,
                     item.spawnCount.max
@@ -1246,7 +1270,7 @@ export class WorldObjectManager {
               // temporary spawnchance
               server.addContainerItem(
                 prop,
-                server.generateItem(item.item, count),
+                server.generateItem(getRandomSkin(item.item), count),
                 container
               );
             }
