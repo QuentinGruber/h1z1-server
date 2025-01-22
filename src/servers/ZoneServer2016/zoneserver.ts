064288ded53d26c38236d7a4f9ce52fbc5d3ae06
Ultrax <ujtjax@hotmail.com>
2025-01-22 23:51:56 +0100

    .

--- src/servers/ZoneServer2016/zoneserver.ts
+++ src/servers/ZoneServer2016/zoneserver.ts
@@ -5684,7 +5684,10 @@ export class ZoneServer2016 extends EventEmitter {
     let randomChance = Math.random() * totalChances;
     for (const rew of rewards) {
       if (randomChance < rew.rewardChance) {
-        return { reward: rew.itemDefinitionId, isRare: rew.rewardChance === 1 };
+        return {
+          reward: rew.itemDefinitionId,
+          isRare: rew.rewardChance === 1 || rew.rewardChance === 0.5
+        };
       }
       randomChance -= rew.rewardChance;
     }
