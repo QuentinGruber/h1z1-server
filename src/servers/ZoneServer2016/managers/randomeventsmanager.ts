4f58fc402ff353ae5d290dcd640c34f004b5e881
quentingruber <quentingruber@gmail.com>
2025-03-25 01:55:55 +0100

    create randomeventsmanager

---
+++ src/servers/ZoneServer2016/managers/randomeventsmanager.ts
@@ -0,0 +1,52 @@
+// ======================================================================
+//
+//   GNU GENERAL PUBLIC LICENSE
+//   Version 3, 29 June 2007
+//   copyright (C) 2020 - 2021 Quentin Gruber
+//   copyright (C) 2021 - 2025 H1emu community
+//
+//   https://github.com/QuentinGruber/h1z1-server
+//   https://www.npmjs.com/package/h1z1-server
+//
+//   Based on https://github.com/psemu/soe-network
+// ======================================================================
+
+import { randomInt } from "node:crypto";
+import { ZoneServer2016 } from "../zoneserver";
+
+export class RandomEventsManager {
+  interval?: NodeJS.Timeout;
+  // managed by config
+  // TODO:
+  enabled: boolean = true;
+  constructor(public server: ZoneServer2016) {}
+  start() {
+    if (this.enabled) {
+      this.interval = setInterval(() => this.run(), 600_000);
+    }
+  }
+  stop() {
+    clearInterval(this.interval);
+  }
+
+  spawnRandomAirdrop() {
+    const spg = this.server._spawnGrid[randomInt(100)];
+    const rnd_index = randomInt(spg.spawnPoints.length);
+    const pos = spg.spawnPoints[rnd_index];
+    this.server.spawnAirdrop(pos, false);
+    // TODO: add the cell
+    this.server.sendAlertToAll("Random airdrop");
+    if (!this.server._soloMode) {
+      this.server._db.collection("random_aidrops_logs").insertOne({
+        position: pos,
+        serverId: this.server._worldId
+      });
+    }
+  }
+
+  run() {
+    if (!this.server._airdrop && randomInt(10) === 0) {
+      this.spawnRandomAirdrop();
+    }
+  }
+}
