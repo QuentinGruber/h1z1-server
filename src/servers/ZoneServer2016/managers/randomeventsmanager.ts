02facd64c8f52b12500cf973c520667d6ef6b837
quentingruber <quentingruber@gmail.com>
2025-03-28 00:09:33 +0100

    adjust random airdrops types

--- src/servers/ZoneServer2016/managers/randomeventsmanager.ts
+++ src/servers/ZoneServer2016/managers/randomeventsmanager.ts
@@ -15,6 +15,11 @@ import { randomInt } from "node:crypto";
 import { ZoneServer2016 } from "../zoneserver";
 import { getCellName } from "../../../utils/utils";
 
+interface WeightedItem {
+  value: string;
+  weight: number;
+}
+
 export class RandomEventsManager {
   interval?: NodeJS.Timeout;
   // managed by config
@@ -30,12 +35,34 @@ export class RandomEventsManager {
     clearInterval(this.interval);
   }
 
+  weightedRandom(items: WeightedItem[]) {
+    let random =
+      Math.random() * items.reduce((sum: number, item) => sum + item.weight, 0);
+
+    return (
+      items.find((item) => (random -= item.weight) < 0) as unknown as {
+        value: string;
+        weight: number;
+      }
+    ).value;
+  }
+
   spawnRandomAirdrop() {
     const cellIndex = randomInt(100);
     const spg = this.server._spawnGrid[cellIndex];
     const rnd_index = randomInt(spg.spawnPoints.length);
     const pos = spg.spawnPoints[rnd_index];
-    this.server.spawnAirdrop(pos, "");
+    const airdropTypes: WeightedItem[] = [
+      { value: "Farmer", weight: 15 },
+      { value: "Demolitioner", weight: 5 },
+      { value: "Medic", weight: 25 },
+      { value: "Builder", weight: 25 },
+      { value: "Fighter", weight: 10 },
+      { value: "Supplier", weight: 20 }
+    ];
+    const airdropType = this.weightedRandom(airdropTypes);
+    console.log(airdropType);
+    this.server.spawnAirdrop(pos, airdropType);
     const cellName = getCellName(cellIndex, 10);
     this.server.sendAlertToAll(`Random airdrop on ${cellName}`);
     if (!this.server._soloMode) {
