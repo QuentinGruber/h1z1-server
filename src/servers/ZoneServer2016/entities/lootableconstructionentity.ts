2073239f9ce3acb6f163d50ab88eb455aae5769f
quentingruber <quentingruber@gmail.com>
2025-06-28 19:05:13 +0200

    hide workbenchs that are in a secured shelter

--- src/servers/ZoneServer2016/entities/lootableconstructionentity.ts
+++ src/servers/ZoneServer2016/entities/lootableconstructionentity.ts
@@ -63,9 +63,6 @@ export class LootableConstructionEntity extends BaseLootableEntity {
   /** Determines if the LootableConstructionEntity is a SmeltingEntity or CollectingEntity */
   subEntity?: SmeltingEntity | CollectingEntity;
 
-  /** The guid of the secured shelter the LootableConstructionEntity is inside */
-  isHidden: string = "";
-
   /** Used by DecayManager, determines if the entity will be damaged the next decay tick */
   isDecayProtected: boolean = false;
   isProp: boolean = false;
