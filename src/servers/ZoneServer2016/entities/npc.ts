ab13ca37a4cf06dab87143027ed690d8ced3d619
johnoclockdk <68790186+johnoclockdk@users.noreply.github.com>
2025-07-01 01:56:18 +0200

    update the modelid

--- src/servers/ZoneServer2016/entities/npc.ts
+++ src/servers/ZoneServer2016/entities/npc.ts
@@ -189,7 +189,7 @@ export class Npc extends BaseFullCharacter {
           const lootbag = new Lootbag(
             characterId,
             server.getTransientId(characterId),
-            9391, // modelId for lootbag
+            9581, // modelId for lootbag
             new Float32Array([
               this.state.position[0] + 0.7,
               this.state.position[1],
