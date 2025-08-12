d907fdfeb7b458437fc7ee5c026df36e35e0d6c5
Jason Moonen <60873596+jasonmoonen@users.noreply.github.com>
2025-08-12 13:42:04 +0200

    Migrate crates to their new ids
    This will make sure the crates are at the top of the account inventory

---
+++ src/servers/ZoneServer2016/data/accountCrates.ts
@@ -0,0 +1,18 @@
+import { AccountItems } from "../models/enums";
+
+export const LEGACY_CRATE_CONVERSION: { [oldId: number]: number } = {
+  2761: AccountItems.REWARD_CRATE_SHOWDOWN,
+  1797: AccountItems.REWARD_CRATE_ALPHA_LAUNCH,
+  3207: AccountItems.REWARD_CRATE_PREDATOR,
+  3118: AccountItems.REWARD_CRATE_EZW,
+  2836: AccountItems.REWARD_CRATE_RENEGADE,
+  2939: AccountItems.REWARD_CRATE_WASTELAND,
+  2031: AccountItems.REWARD_CRATE_BR_1,
+  2436: AccountItems.REWARD_CRATE_INVITATIONAL,
+  2009: AccountItems.REWARD_CRATE_WEARABLES,
+  2276: AccountItems.REWARD_CRATE_MARAUDER,
+  2432: AccountItems.REWARD_CRATE_MERCENARY,
+  3501: AccountItems.REWARD_CRATE_RONIN,
+  3569: AccountItems.REWARD_CRATE_INVITATIONAL_2016,
+  2350: AccountItems.REWARD_CRATE_VIGILANTE
+};
