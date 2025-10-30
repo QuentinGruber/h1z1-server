e36b4cb879c153592b8f3f0ef6a346e637a17ae5
Fabian Sätterlund <fabsa627@student.liu.se>
2025-10-30 07:21:36 +0100

    More reliable proximity items + naming

--- src/servers/ZoneServer2016/zonepackethandlers.ts
+++ src/servers/ZoneServer2016/zonepackethandlers.ts
@@ -1987,20 +1987,17 @@ export class ZonePacketHandlers {
     };
     server.killCharacter(client, damageInfo);
   }
-  FirstTimeEventInventoryAccess(
+  CommandTogglePlayerInterfaces(
     server: ZoneServer2016,
     client: Client,
     packet: ReceivedPacket<object>
   ) {
-    client.character.isInInventory = !client.character.isInInventory;
-    if (client.character.isInInventory) {
-      const proximityItems = server.getProximityItems(client);
-      server.sendData<ClientUpdateProximateItems>(
-        client,
-        "ClientUpdate.ProximateItems",
-        proximityItems
-      );
-    }
+    const proximityItems = server.getProximityItems(client);
+    server.sendData<ClientUpdateProximateItems>(
+      client,
+      "ClientUpdate.ProximateItems",
+      proximityItems
+    ); 
   }
   CommandSuicide(
     server: ZoneServer2016,
@@ -3990,8 +3987,8 @@ export class ZonePacketHandlers {
       case "Command.Redeploy":
         this.CommandRedeploy(server, client, packet);
         break;
-      case "FirstTimeEvent.Unknown1":
-        this.FirstTimeEventInventoryAccess(server, client, packet);
+      case "Command.TogglePlayerInterfaces":
+        this.CommandTogglePlayerInterfaces(server, client, packet);
         break;
       case "Items.RequestUseItem":
         this.RequestUseItem(server, client, packet);
