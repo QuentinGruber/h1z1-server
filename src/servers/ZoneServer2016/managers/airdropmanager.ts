65ea664e7dbd808b92f3bf7c0568883da8adc868
quentingruber <quentingruber@gmail.com>
2026-09-12 11:05:13 +0200

    fix existing issues

--- src/servers/ZoneServer2016/managers/airdropmanager.ts
+++ src/servers/ZoneServer2016/managers/airdropmanager.ts
@@ -13,6 +13,9 @@ import {
 import { ZoneClient2016 } from "../classes/zoneclient";
 import { ExplosiveEntity } from "../entities/explosiveentity";
 import { AirdropConfig, AirdropTypeConfig } from "../models/config";
+import { ZombieWalker } from "../entities/zombiewalker";
+
+const debug = require("debug")("airdropManager");
 
 interface DeliveryProgressData {
   progress: number;
@@ -330,10 +333,10 @@ export class AirdropManager {
         );
 
         if (this.spawnZombiesOnLanding) {
-  debug("[AIRDROP] About to spawn zombies");
-  this.spawnAirdropZombies(position);
-  debug("[AIRDROP] Finished spawning zombies");
-}
+          debug("[AIRDROP] About to spawn zombies");
+          this.spawnAirdropZombies(position);
+          debug("[AIRDROP] Finished spawning zombies");
+        }
       },
       Math.floor(tickAtPos + crateDropSpeed)
     );
@@ -501,56 +504,52 @@ export class AirdropManager {
    * Called once the crate has finished its delivery animation.
    */
   spawnAirdropZombies(
-  position: Float32Array,
-  count: number = this.zombiesPerAirdrop,
-  radius: number = this.zombieSpawnRadius
-) {
-  for (let i = 0; i < count; i++) {
-    const angle = (i / count) * Math.PI * 2;
+    position: Float32Array,
+    count: number = this.zombiesPerAirdrop,
+    radius: number = this.zombieSpawnRadius
+  ) {
+    for (let i = 0; i < count; i++) {
+      const angle = (i / count) * Math.PI * 2;
 
-    const spawnPos = new Float32Array([
-      position[0] + Math.cos(angle) * radius,
-      position[1],
-      position[2] + Math.sin(angle) * radius,
-      0
-    ]);
-
-    const modelId =
-      Math.random() < 0.5
-        ? ModelIds.ZOMBIE_MALE_WALKER
-        : ModelIds.ZOMBIE_FEMALE_WALKER;
-
-    const rotation = eul2quat(
-      new Float32Array([
-        0,
-        Math.random() * Math.PI * 2,
+      const spawnPos = new Float32Array([
+        position[0] + Math.cos(angle) * radius,
+        position[1],
+        position[2] + Math.sin(angle) * radius,
         0
-      ])
-    );
+      ]);
 
-    const characterId = this.server.generateGuid();
-    const transientId = this.server.getTransientId(characterId);
+      const modelId =
+        Math.random() < 0.5
+          ? ModelIds.ZOMBIE_MALE_WALKER
+          : ModelIds.ZOMBIE_FEMALE_WALKER;
 
-    const zombie = new ZombieWalker(
-      characterId,
-      transientId,
-      modelId,
-      spawnPos,
-      rotation,
-      this.server
-    );
+      const rotation = eul2quat(
+        new Float32Array([0, Math.random() * Math.PI * 2, 0])
+      );
 
-    this.server._npcs[characterId] = zombie;
+      const characterId = this.server.generateGuid();
+      const transientId = this.server.getTransientId(characterId);
 
-    console.log(
-      `[AIRDROP ZOMBIE] spawned ${characterId} ` +
-      `model=${modelId} ` +
-      `pos=[${spawnPos[0].toFixed(2)}, ` +
-      `${spawnPos[1].toFixed(2)}, ` +
-      `${spawnPos[2].toFixed(2)}]`
-    );
+      const zombie = new ZombieWalker(
+        characterId,
+        transientId,
+        modelId,
+        spawnPos,
+        rotation,
+        this.server
+      );
+
+      this.server._npcs[characterId] = zombie;
+
+      debug(
+        `[AIRDROP ZOMBIE] spawned ${characterId} ` +
+          `model=${modelId} ` +
+          `pos=[${spawnPos[0].toFixed(2)}, ` +
+          `${spawnPos[1].toFixed(2)}, ` +
+          `${spawnPos[2].toFixed(2)}]`
+      );
+    }
   }
-}
 
   broadcastDeliveryInfo(client: ZoneClient2016 | undefined = undefined) {
     for (const [airdropId, airdrop] of this.activeAirdrops.entries()) {
